d0b07ab3e65eaa107b654dff94b6b78958cfcf43
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-11-15 20:39:30 +0530

    initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Your app</h1>
+      </div>
+    );
+  }
+}
