5007105fc7b8ef2feae6293f68a3d3d23259b862
dugan_dillon@yahoo.com <dugan_dillon@yahoo.com>
2018-06-25 12:50:46 -0700

    ok cool close enough for now on the trust list

--- app/components/List/index.js
+++ app/components/List/index.js
@@ -5,26 +5,65 @@ import * as RPC from "../../script/rpc";
 import * as TYPE from "../../actiontypes";
 
 const mapStateToProps = state => {
-  return { ...state };
+  return { ...state.listReducer };
 };
 
 const mapDispatchToProps = dispatch => ({
   GetListDump: returnedData =>
-    dispatch({ type: TYPE.GET_TRUST_LIST, payload: returnedData })
+    dispatch({ type: TYPE.GET_TRUST_LIST, payload: returnedData }),
+  ToggleSortDir: () => dispatch({ type: TYPE.TOGGLE_SORT_DIRECTION })
 });
 
 class List extends Component {
   componentDidMount() {
-    RPC.PROMISE("getnetworktrustkeys", []).then(payload =>
-      this.props.GetListDump(payload.keys)
-    );
+    RPC.PROMISE("getnetworktrustkeys", []).then(payload => {
+      this.props.GetListDump(payload.keys);
+      console.log(this.props);
+    });
     console.log(this.props);
+  }
+
+  buildList() {
+    console.log(this.props);
+
+    if (this.props.trustlist) {
+      let sortableList = [...this.props.trustlist];
+      if (this.props.acc) {
+        sortableList = sortableList.sort(
+          (a, b) => b["interest rate"] - a["interest rate"]
+        );
+      } else {
+        sortableList = sortableList.sort(
+          (a, b) => a["interest rate"] - b["interest rate"]
+        );
+      }
+      return sortableList.map(ele => (
+        <tr key={ele.address.slice(0, 8)} className="ListItem">
+          <td key={ele.address.slice(0, 9)}>{ele.address}</td>
+          <td key={ele.address.slice(0, 10)} className="intrestRate">
+            {ele["interest rate"]}
+          </td>
+        </tr>
+      ));
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>List</h1>
+      <div id="listRoot">
+        <h1>Trust List</h1>
+
+        <table id="listbody">
+          <tr id="listhead">
+            <th>
+              <div>Address</div>
+            </th>
+            <th onClick={() => this.props.ToggleSortDir()}>
+              <div>Intrest Rate</div>
+            </th>
+          </tr>
+          <tbody id="tablebody">{this.buildList()}</tbody>
+        </table>
       </div>
     );
   }
--- app/reducers/listReducer.js
+++ app/reducers/listReducer.js
@@ -1,13 +1,20 @@
 import * as TYPE from "../actiontypes";
 
-const initialState = {};
+const initialState = {
+  acc: true
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case TYPE.GET_TRUST_LIST:
       return {
         ...state,
-        ...action.payload
+        trustlist: [...action.payload]
+      };
+    case TYPE.TOGGLE_SORT_DIRECTION:
+      return {
+        ...state,
+        acc: !state.acc
       };
     default:
       return state;
