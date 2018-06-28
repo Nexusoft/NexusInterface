f80b1740f250fa2878395afd5bbf4446301386f9
Kendal Cormany <kendal.cormany@nexus.io>
2018-06-28 11:15:21 -0700

    Just a checkin commit for 6-27

--- app/components/Terminal/index.js
+++ app/components/Terminal/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "./style.css";
+import { timingSafeEqual } from "crypto";
+import * as RPC from "../../script/rpc";
 
 export default class Terminal extends Component {
 
@@ -10,11 +12,13 @@ export default class Terminal extends Component {
       //vars go here
       consoleoutput: [],
       deamonoutput: [],
+      currentInput: "Input commands here, input help for list of commands",
       textinputstyle:{
         width: "80%",
         backgroundColor: "black",
         color: "gray"
       }
+      
     }
 
   }  
@@ -26,9 +30,74 @@ export default class Terminal extends Component {
     });
   }
 
-  processInput()
+  processOutput()
   {
+    let ffff = ["aaa","bbbb","cccc","dddddd","fffff","gggggg"];
+    let num = 0;
+    return  this.state.consoleoutput.map((i) => 
+    {
+      num++;
+      return (
+        <div key = {num}>
+        {i}
+        </div>
+      )
+    });
+  
+  }
 
+  processInput
+  {
+    let ffff = ["aaa","bbbb","cccc","dddddd","fffff","gggggg"];
+    let ddd = [...this.state.consoleoutput];
+    console.log(this.state.currentInput);
+    RPC.PROMISE(this.state.currentInput, []).then(payload => {
+      console.log(payload);
+      for (let aaa in payload)
+      {
+        //ddd.push(aaa + ": " + payload[aaa]);
+        if (typeof payload[aaa] === "object")
+          {
+            ddd.push(aaa + ": " );
+          }
+          else{
+            ddd.push(aaa + ": " + payload[aaa]);
+          }
+          //outputcall.appendChild(line);
+          //If it is a object then we need to display ever var on a new line.
+          if (typeof payload[aaa] === "object"){
+            for (let interalres in payload[aaa])
+            {
+             // let line = document.createElement("div");
+              ddd.push('       ' + interalres + ":" + payload[aaa][interalres]);
+              //outputcall.appendChild(line);
+            }
+          }
+
+      }this.setState(
+      {
+        consoleoutput: ddd,
+        currentInput:"Input commands here, input help for list of commands"
+      }
+    );
+    });
+   //ddd.concat(ffff);
+   //ddd.push(ffff[0]);
+   console.log(ddd)
+    
+    console.log(this.state);
+
+   
+
+
+  }
+
+  _handleKeyPress = (e) => {
+    
+    if (e.key === 'Enter') {
+      e.target.value = "";
+      this.processInput()
+    }
   }
 
   textInputOnFocus()
@@ -55,6 +124,14 @@ export default class Terminal extends Component {
     );
   }
 
+  onInputfieldChange = (e) =>
+  {
+    console.log(e.target.value);
+    this.setState(
+      {
+        currentInput: e.target.value
+      });
+  }
 
   render() {
     let div1style = 
@@ -65,21 +142,21 @@ export default class Terminal extends Component {
       backgroundColor:"black",
     };
     return (
-      <div id="nxs-console-module" style={{height:'100%', overflow:'hidden'}}>
-      RPC Console:
-      <button id={"nxs-console-resetbutton"}  onClick={() => this.resetnexusrpcconsole()}> Reset</button>
-      <div id="nxs-console-output" style={div1style}>
-           {this.props.consoleoutput}
-          <div id="nxs-console-input" style={{textAlign: "left"}}>
-              <input id="input-text" style={{...this.state.textinputstyle}} type="text" placeholder="Input commands here, input help for list of commands" onFocus={() => this.textInputOnFocus()} onBlur={ () => this.textInputOnBlur()}/>
-              <button id="input-submit" style={{width: "18%", background: "black", color:"white", borderColor:"red"}} type="submit" value="Execute" onClick={() => this.processInput()}> </button>
-          </div>
+      <div id="nxs-console-module" style={{height:'100%', width:'100%', overflow:'hidden'}}>
+        RPC Console:
+        <button id={"nxs-console-resetbutton"}  onClick={() => this.resetnexusrpcconsole()}> Reset</button>
+        <div id="nxs-console-output" style={div1style}>
+            {this.processOutput()}
+            <div id="nxs-console-input" style={{textAlign: "left"}}>
+                <input id="input-text" style={{...this.state.textinputstyle}} type="text" value={this.currentInput} placeholder="Input commands here, input help for list of commands" onFocus={() => this.textInputOnFocus()} onBlur={ () => this.textInputOnBlur()} onChange={this.onInputfieldChange} onKeyPress={this._handleKeyPress}/>
+                <button id="input-submit" style={{width: "18%", background: "black", color:"white", borderColor:"red"}} type="submit" value="Execute" onClick={() => this.processInput()}>Execute</button>
+            </div>
+        </div>
+        Deamon Output:
+        <div id="nxs-deamon" style={{textAlign:'left', height:'75%', overflowY:'scroll', backgroundColor:'black'}}>
+          {this.props.deamonoutput}
+        </div>
       </div>
-      Deamon Output:
-      <div id="nxs-deamon" style={{textAlign:'left', height:'75%', overflowY:'scroll', backgroundColor:'black'}}>
-      {this.props.deamonoutput}
-      </div>
-  </div>
     );
   }
 }
