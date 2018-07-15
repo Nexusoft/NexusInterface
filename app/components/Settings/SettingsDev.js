42f5fd5200d348dd61210e39406bec81d12924db
clint-nexus <clint@nexus.io>
2018-07-14 21:22:11 -0500

    Add settings html to respective components

--- app/components/Settings/SettingsDev.js
+++ app/components/Settings/SettingsDev.js
@@ -5,9 +5,20 @@ import styles from "./style.css";
 export default class SettingsDev extends Component {
   render() {
     return (
-      <div>
-        Settings Dev
-      </div>
+      <section id="development">
+
+        <form className="aligned">
+
+          <div className="field">
+            <label htmlFor="devmode">Developer Mode</label>
+            <input id="devmode" type="checkbox" className="switch" onChange="updateDeveloperMode(this)" data-tooltip="Development mode enables advanced features to aid in development. After enabling the wallet must be closed and reopened to enable those features"/>
+          </div>
+
+          <div className="clear-both"></div>
+
+        </form>
+
+      </section>
     );
   }
 }
