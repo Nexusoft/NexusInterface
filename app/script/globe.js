348858d27001f2669558e8e00ca320e634d63762
bryan.hallmark@nexus.io <hallmark.bryan@eideticmind.com>
2018-08-02 22:28:27 -0400

    globe  + background

--- app/script/globe.js
+++ app/script/globe.js
@@ -12,7 +12,7 @@
  */
 
 import * as THREE from "three";
-import world from "../images/world.svg";
+import world from "../images/world.jpg";
 var DAT = DAT || {};
 
 export default (DAT.Globe = function(container, opts) {
