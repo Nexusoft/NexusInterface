1951ee3dd60369a2c19a4b2a6eacdb29c81124b7
resistance-is-futile <brian.smith@nexus.io>
2018-08-22 14:16:31 -0700

    Removing Contactlist.js in the next commit, moving functionality into the index.js file for the addressbook

--- app/components/Addressbook/index.js
+++ app/components/Addressbook/index.js
@@ -25,6 +25,56 @@ const mapDispatchToProps = dispatch => ({});
 
 class Addressbook extends Component {
 
+  // build() 
+  // {
+  //   if (this.props.contacts) {
+  //     const contacts = Object.keys(this.props.contacts).sort();
+  //     return contacts.map(function(item, i){
+  //       if (i === 0)
+  //         return null;
+  //       let contact = this.props.contacts[item];
+  //       return (
+  //       <li key={i} onClick={() => this.select(item)}>
+  //         <span className="contact-avatar">
+  //           <svg viewBox="0 0 100 100">
+  //             <text x='50' y='50' dy='.35em'>
+  //               {this.getinitial(item)}
+  //             </text>
+  //           </svg>
+  //         </span>
+  //         <span className="contact-name">{(item === "" ? "My Addresses" : item)}</span>
+  //         <span className="contact-phone">{contact.phoneNum}</span>
+  //         <span className="contact-addresses">{Object.keys(contact.notMine).length} Addresses</span>
+  //       </li>
+  //       )
+  //     }.bind(this));
+  //   }
+  // }
+
+  // getinitial(name) 
+  // {
+  //   if (name && name.length >= 1)
+  //     return name.charAt(0);
+  //   return "M";  // My Addresses
+  // }
+
+  // select = (item) => {
+  //   // get this by item name instead of index since we sorted the names list
+  //   let index = Object.keys(this.props.contacts).indexOf(item);
+  //   this.props.onSelect(index);
+  // }
+
+  // render() {
+  //   return (
+  //     <div id="addressbook-contacts">
+  //       <ul>
+  //         {this.build()}
+  //       </ul>
+  //     </div>
+  //   );
+  // }
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
   static contextTypes = {
     router: React.PropTypes.object
   };
