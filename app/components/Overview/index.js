19a6b07300159b9b31b5c5c451bec33f0ddce9d1
resistance-is-futile <brian.smith@nexus.io>
2018-09-04 14:00:44 -0700

    Fixed MIT license

--- app/components/Overview/index.js
+++ app/components/Overview/index.js
@@ -150,7 +150,31 @@ class Overview extends Component {
   returnLicenseModalInternal() {
     let internalString = [];
 
-    internalString.push("MIT LICENSE GOES HERE");
+    internalString.push(`The MIT License (MIT)`);
+    internalString.push(<br key="br1" />);
+
+    internalString.push(`Copyright (c) 2015-present C. T. Lin`);
+    internalString.push(<br key="br1" />);
+    
+    internalString.push(` Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:`);
+    internalString.push(<br key="br1" />);
+    
+    internalString.push(`The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.`);
+    internalString.push(<br key="br1" />);
+
+     internalString.push(`THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.`);
     internalString.push(<br key="br1" />);
     internalString.push(
       <button
