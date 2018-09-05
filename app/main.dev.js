22abd9657ec6d6827c721896cfcaf6fcb5d154b4
resistance-is-futile <brian.smith@nexus.io>
2018-09-05 11:34:42 -0700

    Starting work on mmdb file location

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,15 +1,3 @@
-/* eslint global-require: 1*/
-
-/**
- * This module executes inside of electron's main process. You can start
- * electron renderer process from here and communicate with the other processes
- * through IPC.
- *
- * When running `npm run build` or `npm run build-main`, this file is compiled to
- * `./app/main.prod.js` using webpack. This gives us some performance wins.
- *
- *
- */
 import { app, BrowserWindow, remote, Tray, Menu, ipcMain } from "electron";
 import log from "electron-log";
 import { autoUpdater } from "electron-updater";
@@ -22,63 +10,36 @@ let mainWindow;
 let tray;
 let resizeTimer;
 
-//
 // Global Objects
-//
-
 global.core = core;
 
-//
 // Configure Updater
-//
-
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = "info";
 
-//
 // Enable source map support
-//
-
 if (process.env.NODE_ENV === "production") {
   const sourceMapSupport = require("source-map-support");
   sourceMapSupport.install();
 }
 
-//
-// Enable debugging for development or production with debug flag
-//
-
-// if (
-//   process.env.NODE_ENV === "development" ||
-//   process.env.DEBUG_PROD === "true"
-// ) {
 require("electron-debug")();
 const p = path.join(__dirname, "..", "app", "node_modules");
 require("module").globalPaths.push(p);
-// }
 
-//
 // Enable development tools for REACT and REDUX
-//
-
 const installExtensions = async () => {
   const installer = require("electron-devtools-installer");
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   const extensions = ["REACT_DEVELOPER_TOOLS", "REDUX_DEVTOOLS"];
-
   return Promise.all(
     extensions.map(name => installer.default(installer[name], forceDownload))
   ).catch(console.log);
 };
 
-//
 // Initialize application updater and check for updates
-//
-
 function updateApplication() {
-  //
   // TODO: IMPORTANT: Prior to going live, remove this code and revoke the github token. Feed URL logic only applies with a private github repository
-  //
 
   const data = {
     provider: "github",
@@ -96,10 +57,7 @@ function updateApplication() {
   }
 }
 
-//
 // Set up the icon in the system tray
-//
-
 function setupTray() {
   let trayImage;
 
@@ -219,10 +177,7 @@ function createWindow() {
   });
 }
 
-//
 // Application Startup
-//
-
 app.on("ready", async () => {
   if (
     process.env.NODE_ENV === "development" ||
@@ -230,18 +185,12 @@ app.on("ready", async () => {
   ) {
     await installExtensions();
   }
-
-  core.start();
-
   createWindow();
-
   setupTray();
+  core.start();
 });
 
-//
 // Application Shutdown
-//
-
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     core.stop(function() {
