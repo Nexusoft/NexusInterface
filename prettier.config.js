02f6b1c12e8201e5cd76caabd347caddd2898897
Krysto <quy.p.hoang@gmail.com>
2018-12-01 01:11:51 +0700

    Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: true,
+  overrides: [
+    {
+      files: '*.css',
+      options: {
+        semi: true,
+        singleQuote: false
+      }
+    }
+  ]
+}
