96659ffc879186bd73140f821daca13ce16fb89a
Krysto <quy.p.hoang@gmail.com>
2018-12-01 23:38:59 +0700

    corrected the Prettier setting

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  trailingComma: true,
+  trailingComma: 'es5',
   overrides: [
     {
       files: '*.css',
       options: {
         semi: true,
-        singleQuote: false
-      }
-    }
-  ]
+        singleQuote: false,
+      },
+    },
+  ],
 }
