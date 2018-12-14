17dc3b2171c35dc6a236d56b8548032dd0d14583
Krysto <quy.p.hoang@gmail.com>
2018-12-14 17:33:54 +0700

    use object styles in Button

--- app/components/common/Button.js
+++ app/components/common/Button.js
@@ -4,53 +4,53 @@ import styled from '@emotion/styled';
 import { colors, timing } from 'styles';
 import { lighten, fade } from 'utils/colors';
 
-const Button = styled.button`
-  display: inline-block;
-  border-radius: 4px;
-  padding: 0.8em 1.6em;
-  border: none;
-  outline: none;
-  background: transparent;
-  cursor: pointer;
-  white-space: nowrap;
-  user-select: none;
-  transition-properties: border-color, color;
-  transition-duration: ${timing.normal};
-  /* Default styles */
-  border: 2px solid ${colors.lightGray};
-  color: ${colors.lightGray};
-  &:hover {
-    border-color: ${colors.light};
-    color: ${colors.light};
-  }
+const Button = styled.button(
+  {
+    display: 'inline-block',
+    borderRadius: 4,
+    padding: '0.8em 1.6em',
+    border: 'none',
+    outline: 'none',
+    background: 'transparent',
+    cursor: 'pointer',
+    whiteSpace: 'nowrap',
+    userSelect: 'none',
+    transitionProperties: 'border-color, color',
+    transitionDuration: timing.normal,
+    // Default styles
+    border: `2px solid ${colors.lightGray}`,
+    color: colors.lightGray,
+    '&:hover': {
+      borderColor: colors.light,
+      color: colors.light,
+    },
+  },
 
-  ${({ primary }) =>
-    !!primary &&
-    `
-    border: 2px solid ${colors.primary};
-    color: ${colors.primary};
-    font-weight: bold;
-    transition-properties: border-color, color, box-shadow, text-shadow;
-    &:hover {
-      border-color: ${lighten(colors.primary, 0.3)};
-      color: ${lighten(colors.primary, 0.3)};
-      box-shadow: 0 0 20px ${fade(colors.primary, 0.5)};
-      text-shadow: 0 0 20px ${fade(colors.primary, 0.5)};
-    }
-  `}
+  ({ primary }) =>
+    !!primary && {
+      border: `2px solid ${colors.primary}`,
+      color: colors.primary,
+      fontWeight: 'bold',
+      transitionProperties: 'border-color, color, box-shadow, text-shadow',
+      '&:hover': {
+        borderColor: lighten(colors.primary, 0.3),
+        color: lighten(colors.primary, 0.3),
+        boxShadow: `0 0 20px ${fade(colors.primary, 0.5)}`,
+        textShadow: `0 0 20px ${fade(colors.primary, 0.5)}`,
+      },
+    },
 
-  ${({ blank }) =>
-    !!blank &&
-    `
-    border: none;
-    background-color: transparent;
-    color: ${colors.lightGray};
-    transition-properties: color;
-    &:hover {
-      border: none;
-      color: ${colors.light};
+  ({ blank }) =>
+    !!blank && {
+      border: 'none',
+      backgroundColor: 'transparent',
+      color: colors.lightGray,
+      transitionProperties: 'color',
+      '&:hover': {
+        border: 'none',
+        color: colors.light,
+      },
     }
-  `}
-`;
+);
 
 export default Button;
