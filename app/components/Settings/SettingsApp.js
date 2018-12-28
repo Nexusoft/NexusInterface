07e0d2d94c536d31b39171f83f05ffc6ed9feb1d
demogl0w <Demorio@live.com>
2018-12-27 18:13:17 -0700

    + Sign For 24hr Change, Adressbook String

--- app/components/Settings/SettingsApp.js
+++ app/components/Settings/SettingsApp.js
@@ -770,7 +770,7 @@ class SettingsApp extends Component {
                 type="text"
                 value={this.props.settings.Folder}
                 onChange={e => this.props.SeeFolder(e.target.value)}
-                onClick={e => {
+                onDoubleClick={e => {
                   e.preventDefault();
                   this.getFolder(this.props.settings.Folder[0]);
                 }}
