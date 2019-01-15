75778f24f2f5e1c248a8d8bc99b248670c43fa70
Krysto <quy.p.hoang@gmail.com>
2019-01-15 15:19:54 +0700

    fixed production font issue

--- webpack.config.renderer.prod.js
+++ webpack.config.renderer.prod.js
@@ -80,6 +80,7 @@ export default merge.smart(baseConfig, {
           options: {
             limit: 10000,
             mimetype: 'font/woff2',
+            outputPath: '../fonts',
           },
         },
       },
