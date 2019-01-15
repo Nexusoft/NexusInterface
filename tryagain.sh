63368c7bf249c272c8a8b5db653eb213f3c60cbe
Dillon <dugan_dillon@yahoo.com>
2019-01-15 14:22:52 -0700

    couple of tweaks to try again

--- tryagain.sh
+++ tryagain.sh
@@ -4,12 +4,14 @@ rm -r release
 rm -r app/dist
 rm app/*.map
 rm app/main.prod.js
-sudo apt-get purge -y nexus_wallet_0.8.4
-rm -r ~/.config/Nexus
+sudo apt-get purge -y nexus-wallet
+rm -r ~/.config/Nexus_Wallet_BETA_v0.8.4
 rm -r dll
+echo ""
+echo "Removed old build and associated data."
+echo ""
 npm run package-linux
 sudo apt-get install -y ./release/nexus_wallet_0.8.4_amd64.deb
 echo ""
 echo "Cleared for next attempt!"
 echo ""
-
