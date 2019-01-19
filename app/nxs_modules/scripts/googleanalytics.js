b98528743f1c350b6f506e57ec925982e5880c4a
Kendal Cormany <kendal.cormany@nexus.io>
2019-01-19 16:40:44 -0700

    New Version

--- app/nxs_modules/scripts/googleanalytics.js
+++ app/nxs_modules/scripts/googleanalytics.js
@@ -24,7 +24,7 @@ if (
 GA.SendScreen = function(ScreenTitle) {
   if (GA.active == false) return;
 
-  GA.visitor.screenview(ScreenTitle, 'Nexus Wallet', '0.8.5').send();
+  GA.visitor.screenview(ScreenTitle, 'Nexus Wallet', '0.8.6').send();
   console.log('Sent Screen: ' + ScreenTitle);
 };
 
