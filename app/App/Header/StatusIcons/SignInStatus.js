b294b5f8135c717e1f972f47bcb875e6e99421f3
Krysto <quy.p.hoang@gmail.com>
2019-01-24 15:16:37 +0700

    improved signin status icon

--- app/App/Header/StatusIcons/SignInStatus.js
+++ app/App/Header/StatusIcons/SignInStatus.js
@@ -2,25 +2,61 @@
 // External Dependencies
 import React, { Component } from 'react';
 import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
 import { connect } from 'react-redux';
-import { withTheme } from 'emotion-theming';
+import { push } from 'connected-react-router';
 
 // Internal Dependencies
 import Text from 'components/Text';
 import Tooltip from 'components/Tooltip';
 import StatusIcon from 'components/StatusIcon';
+import { timing } from 'styles';
+import { color } from 'utils';
 
 // Images
 import questionMarkIcon from 'images/question-mark.sprite.svg';
 import lockedIcon from 'images/padlock.sprite.svg';
 import unlockedIcon from 'images/padlock-open.sprite.svg';
 
-@withTheme
-@connect(({ overview: { connections, unlocked_until, staking_only } }) => ({
+const LoginStatusIcon = styled(StatusIcon)(
+  ({ theme }) => ({
+    cursor: 'pointer',
+    color: theme.primary,
+    transitionProperty: 'color, filter',
+    transitionDuration: timing.normal,
+    '&:hover': {
+      color: color.lighten(theme.primary, 0.2),
+      filter: `drop-shadow(0 0 3px ${color.fade(theme.primary, 0.5)})`,
+    },
+  }),
+  ({ danger, theme }) =>
+    danger && {
+      color: theme.danger,
+      '&:hover': {
+        color: color.lighten(theme.danger, 0.2),
+        filter: `drop-shadow(0 0 3px ${color.fade(theme.danger, 0.5)})`,
+      },
+    },
+  ({ dimmed }) =>
+    dimmed && {
+      opacity: 0.7,
+    }
+);
+
+const mapStateToProps = ({
+  overview: { connections, unlocked_until, staking_only },
+}) => ({
   connections,
   unlocked_until,
   staking_only,
-}))
+});
+
+const actionCreators = { push };
+
+@connect(
+  mapStateToProps,
+  actionCreators
+)
 class SignInStatus extends Component {
   signInStatusMessage = () => {
     const { connections, unlocked_until, staking_only } = this.props;
@@ -44,41 +80,54 @@ class SignInStatus extends Component {
     } else if (unlocked_until === 0) {
       return <Text id="Header.WalletLocked" />;
     } else if (unlocked_until >= 0) {
-      if (staking_only) {
-        return (
-          <>
-            <Text id="Header.UnlockedUntil" /> {unlockDate}{' '}
-            <Text id="Header.StakingOnly" />
-          </>
-        );
-      } else {
-        return (
-          <>
-            <Text id="Header.UnlockedUntil" /> {unlockDate}
-          </>
-        );
-      }
+      return (
+        <>
+          <Text id="Header.UnlockedUntil" data={{ unlockDate }} />{' '}
+          {!!staking_only && <Text id="Header.StakingOnly" />}
+        </>
+      );
     }
   };
 
   statusIcon = () => {
-    const { connections, unlocked_until, theme } = this.props;
+    const { connections, unlocked_until } = this.props;
     if (connections === undefined) {
       return <StatusIcon icon={questionMarkIcon} css={{ opacity: 0.7 }} />;
     } else {
       if (unlocked_until === undefined) {
-        return <StatusIcon icon={unlockedIcon} css={{ color: theme.danger }} />;
+        return (
+          <LoginStatusIcon
+            icon={unlockedIcon}
+            onClick={this.goToSecurity}
+            danger
+          />
+        );
       } else if (unlocked_until === 0) {
-        return <StatusIcon icon={lockedIcon} />;
+        return (
+          <LoginStatusIcon icon={lockedIcon} onClick={this.goToSecurity} />
+        );
       } else if (unlocked_until >= 0) {
-        return <StatusIcon icon={unlockedIcon} css={{ opacity: 0.7 }} />;
+        return (
+          <LoginStatusIcon
+            icon={unlockedIcon}
+            onClick={this.goToSecurity}
+            dimmed
+          />
+        );
       }
     }
   };
 
+  goToSecurity = () => {
+    this.props.push('/Settings/Security');
+  };
+
   render() {
     return (
-      <Tooltip.Trigger tooltip={this.signInStatusMessage()}>
+      <Tooltip.Trigger
+        tooltip={this.signInStatusMessage()}
+        css={{ maxWidth: 200 }}
+      >
         {this.statusIcon()}
       </Tooltip.Trigger>
     );
