808ba408e8c5e5b55db295ae0778ea4a62265d0f
Nino00slav <47416094+Nino00slav@users.noreply.github.com>
2019-02-13 06:12:35 +0100

    fix double id error, fee setting button

--- app/App/Settings/Core/FeeSetting.js
+++ app/App/Settings/Core/FeeSetting.js
@@ -87,7 +87,7 @@ class FeeSetting extends React.Component {
               onClick={this.confirmSetTxFee}
               style={{ marginLeft: '1em' }}
             >
-              <Text id="Settings.SetFee" />
+              <Text id="Settings.SetFeeButton" />
             </Button>
           </div>
         )}
