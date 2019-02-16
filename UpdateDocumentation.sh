84c74d7242055837d5979701f0467b8399fd3402
KendalSpikyFish <kendal@spikyfishgames.com>
2019-02-15 17:07:42 -0700

    Added  output text for updating documentation script

--- UpdateDocumentation.sh
+++ UpdateDocumentation.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
+echo ""
+echo "Building MD Documentation"
+echo ""
 npm documentation build app/index.js -f md -o docs/Documentation.md
+echo ""
+echo "Building HTML Documentation"
+echo ""
 npm documentation build app/index.js -f html -o docs
