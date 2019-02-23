6eeaf5608361fb7d56e8687cd727460d44f1a9fa
Krysto <quy.p.hoang@gmail.com>
2019-02-23 11:26:10 +0700

    keep the last active console tab on navigating

--- app/nxs_modules/reducers/ui/index.js
+++ app/nxs_modules/reducers/ui/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 
 import addressBook from './addressBook';
 import settings from './settings';
+import console from './console';
 
-export default combineReducers({ addressBook, settings });
+export default combineReducers({ addressBook, settings, console });
