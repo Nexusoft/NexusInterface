06f341a51ea891f8e4c5de3a0a00f7025c12c4ae
KendalSpikyFish <kendal@spikyfishgames.com>
2019-03-05 16:31:39 -0700

    Set Version to 0.9.0-alpha2 and added newest tritium core.

--- BuildStandalone-Linux.sh
+++ BuildStandalone-Linux.sh
@@ -5,13 +5,13 @@ rm -r app/dist
 rm app/*.map
 rm app/main.prod.js
 sudo apt-get purge -y nexus-wallet
-rm -r ~/.config/Nexus_Wallet_BETA_v0.8.9
+rm -r ~/.config/Nexus_Wallet_BETA_v0.9.0-alpha2
 rm -r dll
 echo ""
 echo "Removed old build and associated data."
 echo ""
 npm run package-linux
-sudo apt-get install -y ./release/nexus_wallet_0.8.9_amd64.deb
+sudo apt-get install -y ./release/nexus_wallet_0.9.0-alpha2_amd64.deb
 echo ""
 echo "Cleared for next attempt!"
 echo ""
