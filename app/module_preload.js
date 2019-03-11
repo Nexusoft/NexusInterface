d1b5bb2582a49e9266b1220e7f2d1ddac4f79ec5
Krysto <quy.p.hoang@gmail.com>
2019-03-11 16:38:50 +0700

    Module API passed through preload script

--- app/module_preload.js
+++ app/module_preload.js
@@ -1,5 +1,25 @@
-const { ipcRenderer } = require('electron');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const redux = require('redux');
+const reactRedux = require('react-redux');
+const core = require('@emotion/core');
+const styled = require('@emotion/styled');
+const victory = require('victory');
+const { ipcRenderer, clipboard } = require('electron');
 
-global.sendToHost = (...args) => ipcRenderer.sendToHost(...args);
-
-global.listenToHost = (channel, listener) => ipcRenderer.on(channel, listener);
+global.Nexus = {
+  version: MODULE_API_VERSION,
+  libraries: {
+    React,
+    ReactDOM,
+    redux,
+    reactRedux,
+    emotion: { core, styled },
+    victory,
+  },
+  utilities: {
+    copyToClipboard: (...args) => clipboard.writeText(...args),
+  },
+  sendMessage: (...args) => ipcRenderer.sendToHost(...args),
+  listenToMessage: (...args) => ipcRenderer.on(...args),
+};
