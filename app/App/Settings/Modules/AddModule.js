8957907ec4d8481b4e8c957983f91e3e68c1e17d
Krysto <quy.p.hoang@gmail.com>
2019-03-27 15:08:47 +0700

    separated browse files and browse directories

--- app/App/Settings/Modules/AddModule.js
+++ app/App/Settings/Modules/AddModule.js
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 // Internal
 import Icon from 'components/Icon';
 import FieldSet from 'components/FieldSet';
+import Button from 'components/Button';
 import { consts, timing } from 'styles';
 import * as color from 'utils/color';
 import plusCircleIcon from 'images/plus.sprite.svg';
@@ -15,28 +16,26 @@ import installModule from './installModule';
 const AddModuleComponent = styled(FieldSet)(
   ({ theme }) => ({
     textAlign: 'center',
-    cursor: 'pointer',
     fontSize: '.9em',
-    color: theme.mixer(0.5),
+    color: theme.mixer(0.75),
+    borderColor: theme.mixer(0.375),
     margin: '0 0 1em',
     outline: 'none',
     transitionProperty: 'color, border-color, background-color',
     transitionDuration: timing.normal,
     '&:hover': {
       color: theme.mixer(0.75),
-      borderColor: theme.mixer(0.375),
     },
   }),
   ({ active, theme }) =>
     active && {
       background: color.fade(theme.foreground, 0.9),
-      color: theme.mixer(0.75),
-      borderColor: theme.mixer(0.375),
+      color: theme.mixer(0.875),
+      borderColor: theme.mixer(0.5),
     }
 );
 
 const InnerMessage = styled.div({
-  pointerEvents: 'none',
   height: consts.lineHeight * 2 + 'em',
   display: 'flex',
   alignItems: 'center',
@@ -47,8 +46,28 @@ class AddModule extends React.Component {
   browseFiles = () => {
     remote.dialog.showOpenDialog(
       {
-        title: 'Select module directory or archive',
-        properties: ['openFile', 'openDirectory'],
+        title: 'Select module archive file',
+        properties: ['openFile'],
+        filters: [
+          {
+            name: 'ZIP Archive',
+            extensions: ['zip'],
+          },
+        ],
+      },
+      paths => {
+        if (paths && paths[0]) {
+          installModule(paths[0]);
+        }
+      }
+    );
+  };
+
+  browseDirectories = () => {
+    remote.dialog.showOpenDialog(
+      {
+        title: 'Select module directory',
+        properties: ['openDirectory'],
       },
       paths => {
         if (paths && paths[0]) {
@@ -59,6 +78,7 @@ class AddModule extends React.Component {
   };
 
   getFilesFromEvent = event => {
+    if (!event || !event.dataTransfer) return [];
     if (event.type === 'drop') {
       return Array.from(event.dataTransfer.files);
     } else {
@@ -81,13 +101,13 @@ class AddModule extends React.Component {
         {({ getRootProps, getInputProps, isDragActive }) => (
           <AddModuleComponent
             {...getRootProps()}
+            onClick={() => {}}
             legend={
               <>
                 <Icon icon={plusCircleIcon} />
                 <span className="v-align space-left">Add Module</span>
               </>
             }
-            onClick={this.browseFiles}
             active={isDragActive}
           >
             <InnerMessage>
@@ -95,7 +115,16 @@ class AddModule extends React.Component {
                 <div>Drop here to install</div>
               ) : (
                 <div>
-                  <div>Select module directory or .zip file</div>
+                  <div>
+                    Select module{' '}
+                    <Button skin="hyperlink" onClick={this.browseFiles}>
+                      archive
+                    </Button>{' '}
+                    or{' '}
+                    <Button skin="hyperlink" onClick={this.browseDirectories}>
+                      directory
+                    </Button>
+                  </div>
                   <div>or drag and drop it here</div>
                 </div>
               )}
