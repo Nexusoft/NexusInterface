c820807c9683f7006ece8fe9f2ccdd0e4bc09e3b
Krysto <quy.p.hoang@gmail.com>
2019-07-03 18:30:04 +0700

    removed some unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 const stage0Preset = [
   // Stage 0
-  '@babel/plugin-proposal-function-bind',
+  // '@babel/plugin-proposal-function-bind',
   // Stage 1
   '@babel/plugin-proposal-export-default-from',
-  '@babel/plugin-proposal-logical-assignment-operators',
+  // '@babel/plugin-proposal-logical-assignment-operators',
   ['@babel/plugin-proposal-optional-chaining', { loose: false }],
   ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-  ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-  '@babel/plugin-proposal-do-expressions',
+  // ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+  // '@babel/plugin-proposal-do-expressions',
   // Stage 2
   ['@babel/plugin-proposal-decorators', { legacy: true }],
-  '@babel/plugin-proposal-function-sent',
+  // '@babel/plugin-proposal-function-sent',
   '@babel/plugin-proposal-export-namespace-from',
   '@babel/plugin-proposal-numeric-separator',
   '@babel/plugin-proposal-throw-expressions',
@@ -33,7 +33,7 @@ const devPlugins = [];
 const prodPlugins = ['babel-plugin-dev-expression', ...reactOptimizePreset];
 
 module.exports = function(api) {
-  const development = process.env.NODE_ENV !== 'production';
+  const development = process.env.NODE_ENV === 'development';
   api.cache(true);
 
   return {
