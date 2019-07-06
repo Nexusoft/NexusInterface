092e785bb68323539a84b07ba184bfedcdf10d18
Krysto <quy.p.hoang@gmail.com>
2019-07-06 10:38:52 +0700

    auto rebuild main.dev.js before running dev script

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -176,15 +176,5 @@ export default merge.smart(baseConfig, {
       verbose: true,
       disableDotRule: false,
     },
-    before() {
-      console.log('Staring Main Process...');
-      spawn('npm', ['run', 'start-main-dev'], {
-        shell: true,
-        env: process.env,
-        stdio: 'inherit',
-      })
-        .on('close', code => process.exit(code))
-        .on('error', spawnError => console.error(spawnError));
-    },
   },
 });
