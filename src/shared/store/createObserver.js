76354af99aee46c797853568ef4d25f7dd483017
Krysto <quy.p.hoang@gmail.com>
2019-07-06 16:38:15 +0700

    added observerStore method

---
+++ src/shared/store/createObserver.js
@@ -0,0 +1,17 @@
+const createObserver = store => (select, onChange) => {
+  let currentState;
+
+  function handleChange() {
+    const nextState = select(store.getState());
+    if (nextState !== currentState) {
+      currentState = nextState;
+      onChange(currentState);
+    }
+  }
+
+  const unsubscribe = store.subscribe(handleChange);
+  handleChange();
+  return unsubscribe;
+};
+
+export default createObserver;
--- src/shared/store/index.js
+++ src/shared/store/index.js
@@ -1,4 +1,10 @@
 import { configureStore, history } from './configureStore';
+import createObserver from './createObserver';
 
 export { configureStore, history };
-export default configureStore();
+
+const store = configureStore();
+
+export const observeStore = createObserver(store);
+
+export default store;
