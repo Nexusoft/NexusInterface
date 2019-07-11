eb9a61910ebda774c81c6eb470cac34664e295a8
Krysto <quy.p.hoang@gmail.com>
2019-07-11 16:40:34 +0700

    Revert "fixed the error when sending multiple commands"
    This reverts commit 01f666c0bf5a70c95997a25400b3f51b29da7114.

--- src/shared/actions/accountActionCreators.js
+++ src/shared/actions/accountActionCreators.js
@@ -1,31 +1,23 @@
 import * as RPC from 'lib/rpc';
 import * as TYPE from './actiontypes';
 
-// Nexus core can only handle an amount of RPC requests at the same time
-// So we make the requests in batches to avoid the errors
-async function batchPromises(list, toPromise, batchSize = 3) {
-  let results = [];
-  while (list.length) {
-    const batch = list.splice(0, batchSize);
-    results = results.concat(await Promise.all(batch.map(toPromise)));
-  }
-  return results;
-}
-
 export const loadMyAccounts = () => async dispatch => {
   const accList = await RPC.PROMISE('listaccounts', []);
-  const accounts = Object.keys(accList);
-  const addrList = await batchPromises(accounts, account =>
-    RPC.PROMISE('getaddressesbyaccount', [account])
+
+  const addrList = await Promise.all(
+    Object.keys(accList).map(account =>
+      RPC.PROMISE('getaddressesbyaccount', [account])
+    )
   );
 
-  let addresses = [];
-  for (let addrs of addrList) {
-    addresses = addresses.concat(addrs);
-  }
-  const validations = await batchPromises(addresses, address =>
-    RPC.PROMISE('validateaddress', [address])
+  const validateAddressPromises = addrList.reduce(
+    (list, element) => [
+      ...list,
+      ...element.map(address => RPC.PROMISE('validateaddress', [address])),
+    ],
+    []
   );
+  const validations = await Promise.all(validateAddressPromises);
 
   const accountList = [];
   validations.forEach(e => {
