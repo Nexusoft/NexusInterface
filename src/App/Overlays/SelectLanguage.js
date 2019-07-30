8f9168b2633bbfa70f38e60b296c3ec1c8077b01
Krysto <quy.p.hoang@gmail.com>
2019-07-30 19:03:34 +0700

    completed SelectLanguage screen

--- src/App/Overlays/SelectLanguage.js
+++ src/App/Overlays/SelectLanguage.js
@@ -1,14 +1,90 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 import Button from 'components/Button';
+import languages from 'data/languages';
+import { timing } from 'styles';
+import * as color from 'utils/color';
+import { UpdateSettings } from 'lib/settings';
 
 import FullScreen from './FullScreen';
 
-const SelectLanguage = ({}) => (
-  <FullScreen
-    header={__('Language')}
-    footer={<Button skin="primary">Select</Button>}
-  />
+const Flag = styled.img({
+  marginRight: '.5em',
+  verticalAlign: 'middle',
+});
+
+const Language = styled.div(
+  ({ first, theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '1em 0',
+    borderRadius: 2,
+    border: `1px solid transparent`,
+    borderTop: first ? undefined : `1px solid ${theme.mixer(0.125)}`,
+    transitionProperty: 'background-color, color, border-color',
+    transitionDuration: timing.normal,
+    cursor: 'pointer',
+    '&:hover': {
+      background: theme.background,
+    },
+  }),
+  ({ selected, theme }) =>
+    selected && {
+      '&, &:hover': {
+        borderColor: theme.primary,
+        background: color.fade(theme.primary, 0.5),
+        color: theme.primaryAccent,
+      },
+    }
 );
+
+class SelectLanguage extends React.Component {
+  state = {
+    selection: 'en',
+  };
+
+  selectLanguage = lang => {
+    this.setState({ selection: lang });
+  };
+
+  proceed = () => {
+    UpdateSettings({ locale: this.state.selection });
+    location.reload();
+  };
+
+  render() {
+    return (
+      <FullScreen
+        width={500}
+        header={__('Language')}
+        footer={
+          <Button
+            wide
+            uppercase
+            skin="primary"
+            style={{ fontSize: 16 }}
+            onClick={this.proceed}
+          >
+            {__('Select language')}
+          </Button>
+        }
+      >
+        {languages.map((lang, i) => (
+          <Language
+            key={lang.code}
+            selected={lang.code === this.state.selection}
+            first={i === 0}
+            onClick={() => this.selectLanguage(lang.code)}
+          >
+            <Flag src={lang.flag} />
+            {lang.name}
+          </Language>
+        ))}
+      </FullScreen>
+    );
+  }
+}
 
 export default SelectLanguage;
