4e893764fc018ede38e9fd5fe47a7778cab7d074
Krysto <quy.p.hoang@gmail.com>
2019-09-10 14:43:05 +0700

    added tooltips for balances

--- src/App/UserPage/Balances.js
+++ src/App/UserPage/Balances.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from '@emotion/styled';
 
+import Tooltip from 'components/Tooltip';
+import Icon from 'components/Icon';
 import { switchUserTab } from 'actions/ui';
 import { getBalances } from 'actions/core';
 import { observeStore } from 'store';
+
+import questionIcon from 'images/question-mark-circle.sprite.svg';
 
 const BalancesWrapper = styled.div(({ theme }) => ({
   maxWidth: 300,
@@ -13,6 +17,11 @@ const BalancesWrapper = styled.div(({ theme }) => ({
   paddingLeft: 30,
   color: theme.mixer(0.75),
 }));
+
+const QuestionMark = styled(Icon)({
+  fontSize: '.8em',
+  marginLeft: 6,
+});
 
 const Line = styled.div(
   {
@@ -71,28 +80,69 @@ export default class Balances extends React.Component {
             <div>{total} NXS</div>
           </Line>
           <Line>
-            <div>{__('Available')}</div>
+            <div>
+              <span className="v-align">{__('Available')}</span>
+              <Tooltip.Trigger
+                tooltip={__(
+                  'The current balance across all NXS accounts that is available to be spent'
+                )}
+              >
+                <QuestionMark icon={questionIcon} />
+              </Tooltip.Trigger>
+            </div>
             <div>{balances.available} NXS</div>
           </Line>
           <Line>
-            <div>{__('Pending')}</div>
+            <div>
+              <span className="v-align">{__('Pending')}</span>
+              <Tooltip.Trigger
+                tooltip={__(
+                  'The sum of all debit and coinbase transactions made to your NXS accounts that are confirmed but have not yet been credited. This does NOT include immature and unconfirmed amounts'
+                )}
+              >
+                <QuestionMark icon={questionIcon} />
+              </Tooltip.Trigger>
+            </div>
             <div>{balances.pending} NXS</div>
           </Line>
           <Line>
-            <div>{__('Unconfirmed')}</div>
+            <div>
+              <span className="v-align">{__('Unconfirmed')}</span>
+              <Tooltip.Trigger
+                tooltip={__(
+                  'The sum of all debit transactions made to your NXS accounts that are not confirmed, or credits you have made to your accounts that are not yet confirmed (not yet included in a block)'
+                )}
+              >
+                <QuestionMark icon={questionIcon} />
+              </Tooltip.Trigger>
+            </div>
             <div>{balances.unconfirmed} NXS</div>
           </Line>
           <Line>
-            <div>{__('Staking')}</div>
+            <div>
+              <span className="v-align">{__('Staking')}</span>
+              <Tooltip.Trigger
+                tooltip={__(
+                  'The amount of NXS currently staked in the trust account'
+                )}
+              >
+                <QuestionMark icon={questionIcon} />
+              </Tooltip.Trigger>
+            </div>
             <div>{balances.stake} NXS</div>
           </Line>
           <Line>
-            <div>{__('Immature mined')}</div>
+            <div>
+              <span className="v-align">{__('Immature')}</span>
+              <Tooltip.Trigger
+                tooltip={__(
+                  'The sum of all coinbase transactions that have not yet reached maturity'
+                )}
+              >
+                <QuestionMark icon={questionIcon} />
+              </Tooltip.Trigger>
+            </div>
             <div>{balances.immature_mined} NXS</div>
-          </Line>
-          <Line>
-            <div>{__('Immature stake')}</div>
-            <div>{balances.immature_stake} NXS</div>
           </Line>
         </BalancesWrapper>
       )
