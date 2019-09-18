b2c9bffee6f998924b4ba572a052b2d56d424a30
Krysto <quy.p.hoang@gmail.com>
2019-09-18 22:25:31 +0700

    simple contract content by operation

--- src/App/TransactionsTritium/Contract.js
+++ src/App/TransactionsTritium/Contract.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import { formatNumber } from 'lib/intl';
+import { consts } from 'styles';
 
 const ContractComponent = styled.div({
   padding: '1em',
@@ -23,10 +24,149 @@ const ContractDelta = styled.div(({ theme, negative }) => ({
 
 const OpCode = styled.span({
   fontWeight: 'bold',
+  textTransform: 'uppercase',
 });
 
+const AddressComponent = styled.span({
+  fontFamily: consts.monoFontFamily,
+});
+
+const Address = ({ children }) => {
+  if (!children || typeof children !== 'string' || children.length <= 11)
+    return children;
+  return (
+    <AddressComponent>
+      {children.slice(0, 6)}...{children.slice(-5)}
+    </AddressComponent>
+  );
+};
+
 const contractContent = contract => {
-  return <OpCode>{contract.OP}</OpCode>;
+  switch (contract.OP) {
+    case 'WRITE': {
+      return (
+        <>
+          <OpCode>Write</OpCode> to <Address>{contract.address}</Address>
+        </>
+      );
+    }
+
+    case 'APPEND': {
+      return (
+        <>
+          <OpCode>Append</OpCode> to <Address>{contract.address}</Address>
+        </>
+      );
+    }
+
+    case 'CREATE': {
+      return (
+        <>
+          <OpCode>Create</OpCode> {contract.object_type} =>{' '}
+          <Address>{contract.address}</Address>
+        </>
+      );
+    }
+
+    case 'TRANSFER': {
+      return (
+        <>
+          <OpCode>Transfer</OpCode> <Address>{contract.address}</Address> to{' '}
+          <Address>{contract.destination}</Address>
+        </>
+      );
+    }
+
+    case 'CLAIM': {
+      return (
+        <>
+          <OpCode>Claim</OpCode> <Address>{contract.address}</Address>
+        </>
+      );
+    }
+
+    case 'COINBASE': {
+      return (
+        <>
+          <OpCode>Coinbase</OpCode>
+        </>
+      );
+    }
+
+    case 'TRUST': {
+      return (
+        <>
+          <OpCode>Trust</OpCode>
+        </>
+      );
+    }
+
+    case 'GENESIS': {
+      return (
+        <>
+          <OpCode>Genesis</OpCode> <Address>{contract.address}</Address>
+        </>
+      );
+    }
+
+    case 'DEBIT': {
+      return (
+        <>
+          <OpCode>Debit</OpCode> from{' '}
+          {contract.from_name || <Address>{contract.from}</Address>}
+        </>
+      );
+    }
+
+    case 'CREDIT': {
+      return (
+        <>
+          <OpCode>Credit</OpCode> to{' '}
+          {contract.account_name || <Address>{contract.account}</Address>}
+        </>
+      );
+    }
+
+    case 'MIGRATE': {
+      return (
+        <>
+          <OpCode>Migrate</OpCode> to{' '}
+          {contract.account_name || <Address>{contract.account}</Address>}
+        </>
+      );
+    }
+
+    case 'AUTHORIZE': {
+      return (
+        <>
+          <OpCode>Authorize</OpCode> a transaction
+        </>
+      );
+    }
+
+    case 'FEE': {
+      return (
+        <>
+          <OpCode>Fee</OpCode> from{' '}
+          {contract.account_name || <Address>{contract.account}</Address>}
+        </>
+      );
+    }
+
+    case 'LEGACY': {
+      return (
+        <>
+          <OpCode>Legacy Send</OpCode> from{' '}
+          {contract.from_name || <Address>{contract.from}</Address>}
+        </>
+      );
+    }
+
+    default: {
+      return <OpCode>{contract.OP}</OpCode>;
+    }
+  }
+  return;
 };
 
 const negativeOperations = ['DEBIT', 'FEE'];
