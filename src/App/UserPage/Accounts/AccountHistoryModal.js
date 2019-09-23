850e20dd748a4025dcc9904ff6ed4b2939c79c22
Krysto <quy.p.hoang@gmail.com>
2019-09-23 18:36:50 +0700

    added Balances section into Account History modal

--- src/App/UserPage/Accounts/AccountHistoryModal.js
+++ src/App/UserPage/Accounts/AccountHistoryModal.js
@@ -6,10 +6,13 @@ import Modal from 'components/Modal';
 import WaitingMessage from 'components/WaitingMessage';
 import Table from 'components/Table';
 import ContractDetailsModal from 'components/ContractDetailsModal';
+import FieldSet from 'components/FieldSet';
 import { apiPost } from 'lib/tritiumApi';
 import { formatDateTime, formatNumber } from 'lib/intl';
 import { openModal } from 'actions/overlays';
 import { handleError } from 'utils/form';
+
+import { totalBalance } from './utils';
 
 const displayedOperations = ['DEBIT', 'CREDIT', 'FEE'];
 
@@ -78,8 +81,22 @@ const tableColumns = [
   },
 ];
 
-const ContractsTable = styled(Table)(({ theme }) => ({
+const Layout = styled.div({
   height: '100%',
+  display: 'grid',
+  gridTemplateAreas: '"balances" "table"',
+  gridTemplateRows: 'min-content 1fr',
+  rowGap: '1em',
+});
+
+const BalancesFieldSet = styled(FieldSet)({
+  gridArea: 'balances',
+  margin: 0,
+  fontSize: 15,
+});
+
+const ContractsTable = styled(Table)(({ theme }) => ({
+  gridArea: 'table',
   color: theme.foreground,
   overflow: 'auto',
 }));
@@ -158,27 +175,76 @@ class AccountHistoryModal extends React.Component {
               ...
             </WaitingMessage>
           ) : (
-            <ContractsTable
-              data={contracts}
-              columns={tableColumns}
-              defaultPageSize={10}
-              getTrProps={(state, row) => {
-                const contract = row && row.original;
-                return {
-                  onClick: contract
-                    ? () => {
-                        openModal(ContractDetailsModal, {
-                          contract,
-                        });
-                      }
-                    : undefined,
-                  style: {
-                    cursor: 'pointer',
-                    fontSize: 15,
-                  },
-                };
-              }}
-            />
+            <Layout>
+              <BalancesFieldSet legend={__('Account balance')}>
+                <div className="flex space-between">
+                  <div className="text-center">
+                    <div>
+                      <strong>{__('Total')}</strong>
+                    </div>
+                    <div>
+                      {formatNumber(totalBalance(account))} {account.token_name}
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <div>
+                      <strong>{__('Available')}</strong>
+                    </div>
+                    <div>
+                      {formatNumber(account.balance)} {account.token_name}
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <div>
+                      <strong>{__('Pending')}</strong>
+                    </div>
+                    <div>
+                      {formatNumber(account.pending)} {account.token_name}
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <div>
+                      <strong>{__('Unconfirmed')}</strong>
+                    </div>
+                    <div>
+                      {formatNumber(account.unconfirmed)} {account.token_name}
+                    </div>
+                  </div>
+                  {typeof account.stake === 'number' && (
+                    <div className="text-center">
+                      <div>
+                        <strong>{__('Stake')}</strong>
+                      </div>
+                      <div>
+                        {formatNumber(account.stake)} {account.token_name}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </BalancesFieldSet>
+
+              <ContractsTable
+                data={contracts}
+                columns={tableColumns}
+                defaultPageSize={10}
+                getTrProps={(state, row) => {
+                  const contract = row && row.original;
+                  return {
+                    onClick: contract
+                      ? () => {
+                          openModal(ContractDetailsModal, {
+                            contract,
+                          });
+                        }
+                      : undefined,
+                    style: {
+                      cursor: 'pointer',
+                      fontSize: 15,
+                    },
+                  };
+                }}
+              />
+            </Layout>
           )}
         </Modal.Body>
       </Modal>
