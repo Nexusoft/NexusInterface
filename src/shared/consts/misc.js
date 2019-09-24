29c77b2847d4f10530135aadd69d171da3a042d8
Kendal Cormany <kendal.cormany@nexus.io>
2019-09-23 18:01:14 -0700

    Set mainnet activation back till we have a better idea.

--- src/shared/consts/misc.js
+++ src/shared/consts/misc.js
@@ -14,6 +14,6 @@ export const webGLAvailable = (() => {
 
 export const legacyMode = settings.legacyMode === false ? false : true;
 
-// export const tritiumUpgradeTime = Date.now() + 5000;
+export const tritiumUpgradeTime = Date.now() + 5000;
 
-export const tritiumUpgradeTime = new Date(2019, 8, 30).getTime();
+//export const tritiumUpgradeTime = new Date(2019, 8, 24).getTime();
