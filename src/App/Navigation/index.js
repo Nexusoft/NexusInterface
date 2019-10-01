bcac21cf5b3c0ca61e7b0cbb545ae49678399ee3
Kendal Cormany <kendal.cormany@nexus.io>
2019-10-01 16:56:01 -0700

    User Page no longer shows in legacy mode.

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -24,6 +24,7 @@ import addressBookIcon from 'images/address-book.sprite.svg';
 import settingsIcon from 'images/settings.sprite.svg';
 import consoleIcon from 'images/console.sprite.svg';
 import userIcon from 'images/user.sprite.svg';
+import { legacyMode } from 'consts/misc';
 // import shapeshiftIcon from 'images/shapeshift.sprite.svg';
 // import trustListIcon from 'images/trust-list.sprite.svg';
 
@@ -110,9 +111,11 @@ const Navigation = () => (
         {__('Overview')}
       </NavItem>
 
-      <NavItem icon={userIcon} to="/User">
-        {__('User')}
-      </NavItem>
+      {!legacyMode && (
+        <NavItem icon={userIcon} to="/User">
+          {__('User')}
+        </NavItem>
+      )}
 
       <NavItem icon={sendIcon} to="/Send">
         {__('Send NXS')}
