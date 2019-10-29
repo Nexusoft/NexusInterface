adc0394bdb703591b8fd56b2072ff7818c6ae33e
Kendal Cormany <kendal.cormany@nexus.io>
2019-10-28 18:07:18 -0700

    Beta2 Linux Core

--- src/shared/consts/misc.js
+++ src/shared/consts/misc.js
@@ -14,7 +14,9 @@ export const webGLAvailable = (() => {
 
 export const legacyMode = settings.legacyMode === false ? false : true;
 
-export const tritiumUpgradeTime = Date.now() + 5000;
+//export const tritiumUpgradeTime = Date.now() + 5000;
+
+export const tritiumUpgradeTime = new Date(1572325200000);
 
 //export const tritiumUpgradeTime = new Date(2019, 8, 24).getTime();
 
