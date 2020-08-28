780930fb561e494a689fd7bbb9ff658f0e9b8637
Krysto <quy.p.hoang@gmail.com>
2020-08-28 16:00:20 +0700

    added QRModal

---
+++ src/shared/components/QRModal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import QRCode from 'qrcode';
+import { useTheme } from 'emotion-theming';
+
+import Modal from 'components/Modal';
+import NexusAddress from 'components/NexusAddress';
+
+const width = 322;
+
+const QRWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export default function QRModal({ address }) {
+  const theme = useTheme();
+  const [dataURL, setDataURL] = React.useState(null);
+  React.useEffect(() => {
+    QRCode.toDataURL(
+      address,
+      {
+        width,
+        color: { dark: theme.background, light: theme.foreground },
+        margin: 2,
+      },
+      (err, url) => {
+        if (!err) {
+          setDataURL(url);
+        }
+      }
+    );
+  }, []);
+  return (
+    <Modal maxWidth={500}>
+      <Modal.Body>
+        <QRWrapper>
+          {!!dataURL && <img src={dataURL} width={width} />}
+          <NexusAddress address={address} />
+        </QRWrapper>
+      </Modal.Body>
+    </Modal>
+  );
+}
