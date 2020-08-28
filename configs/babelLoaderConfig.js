6b11807af319508df2a3eb13ca39d666ff405364
Krysto <quy.p.hoang@gmail.com>
2020-08-28 13:21:54 +0700

    activated babel plugin optional chaining

--- configs/babelLoaderConfig.js
+++ configs/babelLoaderConfig.js
@@ -4,7 +4,7 @@ const stage0Preset = [
   // Stage 1
   // '@babel/plugin-proposal-export-default-from',
   // '@babel/plugin-proposal-logical-assignment-operators',
-  // ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+  ['@babel/plugin-proposal-optional-chaining', { loose: false }],
   // ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
   // ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
   // '@babel/plugin-proposal-do-expressions',
