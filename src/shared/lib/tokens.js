2034b2a4650a6fabcbefd49dbe288691b4d966da
Krysto <quy.p.hoang@gmail.com>
2020-10-23 21:00:17 +0700

    single ellipsis character for unnamed token

--- src/shared/lib/tokens.js
+++ src/shared/lib/tokens.js
@@ -7,9 +7,9 @@ export const getTokenName = (
   if (token_name) return token_name;
   if (typeof token === 'string') {
     if (markup) {
-      return <span className="dim">{token.substring(0, 3)}...</span>;
+      return <span className="dim">{token.substring(0, 3)}…</span>;
     } else {
-      return token.substring(0, 3) + '...';
+      return token.substring(0, 3) + '…';
     }
   }
   return 'NXS';
