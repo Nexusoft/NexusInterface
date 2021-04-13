422287c1e8b74c1e549b175d43d0db6e5b0f4751
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-04-13 11:07:48 -0700

    Revert "Used if modified since on module api checks to reduce api strain, Fixes #125"
    This reverts commit cdeb89bfca0e5fb281a3ee219057aad1cb198bb7.

--- src/shared/lib/modules/autoUpdate.js
+++ src/shared/lib/modules/autoUpdate.js
@@ -16,9 +16,6 @@ import { history } from 'lib/wallet';
 
 const githubHeaders = {
   Accept: 'application/vnd.github.v3+json',
-  'If-Modified-Since': new Date(
-    new Date().getTime() - 6 * 60 * 60 * 1000
-  ).toUTCString(),
 };
 
 // const getAssetName = (name, version) => `${name}_v${version}.zip`;
@@ -31,10 +28,7 @@ async function getLatestRelease(repo) {
     });
     return response.data;
   } catch (err) {
-    if (err.response.status !== 304) {
-      // 304 = Not modified in the last 6 hours, so nothing to return. However if it is not then throw that error.
-      console.error(err);
-    }
+    console.error(err);
     return null;
   }
 }
