5f2208cec78f92678c219e0a7424e2a125ef71ce
Krysto <quy.p.hoang@gmail.com>
2021-04-22 17:42:32 +0700

    splitted Stats component out

--- src/App/OverviewTritium/index.js
+++ src/App/OverviewTritium/index.js
@@ -2,33 +2,12 @@
 import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
 import GA from 'lib/googleAnalytics';
 
 // Internal
-import { refreshBalances, loadAccounts } from 'lib/user';
-import { getMiningInfo } from 'lib/core';
-import { timing, consts } from 'styles';
-import { observeStore } from 'store';
-import Globe from './Globe';
 import { webGLAvailable } from 'consts/misc';
-
-import {
-  NXSBalanceStat,
-  NXSFiatBalanceStat,
-  FeaturedTokenBalanceStat,
-  IncomingBalanceStat,
-} from './BalanceStats';
-import { PriceStat, MarketCapStat, PctChangeStat } from './MarketStats';
-import { ConnectionsStat, BlockCountStat } from './CoreStats';
-import {
-  StakeRateStat,
-  BlockWeightStat,
-  TrustWeightStat,
-  StakeWeightStat,
-} from './StakingStats';
-import { PrimeDiffStat, HashDiffStat, StakingDiffStat } from './MiningStats';
-import { StatWrapper, StatIcon } from './Stat';
+import Globe from './Globe';
+import Stats from './Stats';
 
 __ = __context('Overview');
 
@@ -36,88 +15,6 @@ const OverviewPage = styled.div({
   width: '100%',
   position: 'relative',
 });
-
-const slideRight = keyframes`
-  0% {
-    opacity: 0;
-    transform: translate(-100px,-50%);
-  }
-  100% {
-    opacity: 1;
-    transform: translate(0,-50%);
-  }
-`;
-
-const slideLeft = keyframes`
-  0% {
-    opacity: 0;
-    transform: translate(100px,-50%);
-  }
-  100% {
-    opacity: 1;
-    transform: translate(0,-50%);
-  }
-`;
-
-const Stats = styled.div(
-  {
-    position: 'absolute',
-    top: '50%',
-    transform: 'translateY(-50%)',
-    whiteSpace: 'nowrap',
-    // I don't like this but its a quick fix for super small screens.
-    '@media (min-height: 10px) and (max-height: 720px)': {
-      fontSize: '75%',
-      '& svg': {
-        transform: 'scale(0.75)',
-      },
-    },
-  },
-  ({ left, compact }) =>
-    left && {
-      textAlign: 'right',
-      right: compact ? 'calc(56% + 40px)' : 'calc(70% + 40px)',
-      animation: `${timing.slow} ${consts.enhancedEaseOut} 0s ${slideRight}`,
-      [StatWrapper]: {
-        gridTemplateAreas: '"content icon"',
-        justifyContent: 'end',
-      },
-    },
-  ({ right, compact }) =>
-    right && {
-      textAlign: 'left',
-      left: compact ? 'calc(56% + 40px)' : 'calc(70% + 40px)',
-      animation: `${timing.slow} ${consts.enhancedEaseOut} 0s ${slideLeft}`,
-      [StatWrapper]: {
-        gridTemplateAreas: '"icon content"',
-        justifyContent: 'start',
-      },
-    }
-);
-
-function useGetBalances() {
-  useEffect(() => {
-    const unobserve = observeStore(
-      ({ user }) => user && user.status,
-      (status) => {
-        status && refreshBalances();
-      }
-    );
-    return unobserve;
-  }, []);
-}
-
-function useGetDifficulty(overviewDisplay) {
-  useEffect(() => {
-    if (overviewDisplay === 'miner') {
-      getMiningInfo();
-      const intervalID = setInterval(getMiningInfo, 50000);
-      return () => {
-        clearInterval(intervalID);
-      };
-    }
-  }, [overviewDisplay]);
-}
 
 function usePrevious(value) {
   const ref = useRef(undefined);
@@ -171,11 +68,7 @@ export default function Overview() {
 
   useEffect(() => {
     GA.SendScreen('Overview');
-    // Load accounts to display token balances if any
-    loadAccounts();
   }, []);
-  useGetBalances();
-  useGetDifficulty(settings.overviewDisplay);
   const { redrawCurves, removeAllPoints } = useUpdateGlobe({
     settings,
     connections,
@@ -201,37 +94,7 @@ export default function Overview() {
         />
       )}
 
-      <Stats left compact={!showingGlobe}>
-        <NXSBalanceStat />
-        {theme.featuredTokenName ? (
-          <FeaturedTokenBalanceStat />
-        ) : (
-          <NXSFiatBalanceStat />
-        )}
-        <IncomingBalanceStat />
-        <PriceStat />
-        <MarketCapStat />
-        <PctChangeStat />
-      </Stats>
-
-      <Stats right compact={!showingGlobe}>
-        <ConnectionsStat />
-        <BlockCountStat />
-        <StakeRateStat />
-        {settings.overviewDisplay === 'miner' ? (
-          <>
-            <PrimeDiffStat />
-            <HashDiffStat />
-            <StakingDiffStat />
-          </>
-        ) : (
-          <>
-            <BlockWeightStat />
-            <TrustWeightStat />
-            <StakeWeightStat />
-          </>
-        )}
-      </Stats>
+      <Stats showingGlobe={showingGlobe} />
     </OverviewPage>
   );
 }
