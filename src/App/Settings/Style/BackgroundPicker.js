599f7a1c03d9b4d51ec363f8a71bc2ccba740583
Krysto <quy.p.hoang@gmail.com>
2021-04-27 09:11:54 +0700

    refactored background picker

--- src/App/Settings/Style/BackgroundPicker.js
+++ src/App/Settings/Style/BackgroundPicker.js
@@ -1,5 +1,6 @@
 // External
-import { Component } from 'react';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import styled from '@emotion/styled';
 
 // Internal
@@ -34,79 +35,57 @@ const Option = styled.label(
     }
 );
 
-/**
- * The Background Picker Element
- *
- * @class BackgroundPicker
- * @extends {Component}
- */
-class BackgroundPicker extends Component {
-  fileInputID = newUID();
-
-  /**
-   * Handle Picking a file
-   *
-   * @memberof BackgroundPicker
-   */
-  handleFilePick = (e) => {
-    if (!!e.target.files.length) {
-      let imagePath = e.target.files[0].path;
-      // Reset the input value, or onChange won't fire next time
-      // if user selects the same file
-      e.target.value = '';
-      if (process.platform === 'win32') {
-        imagePath = imagePath.replace(/\\/g, '/');
-      }
-      updateTheme({ wallpaper: imagePath });
+function handleFilePick(e) {
+  if (!!e.target.files.length) {
+    let imagePath = e.target.files[0]?.path;
+    // Reset the input value, or onChange won't fire next time
+    // if user selects the same file
+    e.target.value = '';
+    if (imagePath && process.platform === 'win32') {
+      imagePath = imagePath.replace(/\\/g, '/');
     }
-  };
-
-  /**
-   * Component's Renderable JSX
-   *
-   * @returns
-   * @memberof BackgroundPicker
-   */
-  render() {
-    const { wallpaper, defaultStyle } = this.props;
-    const customWallpaper =
-      wallpaper !== starryNightBackground &&
-      wallpaper !== cosmicLightBackground;
-    return (
-      <div>
-        <Option
-          onClick={() => updateTheme({ wallpaper: starryNightBackground })}
-          selected={wallpaper === starryNightBackground}
-          style={{ display: 'inline', marginBottom: '.5em' }}
-        >
-          {__('Starry night')}
-        </Option>
-        <Option
-          onClick={() => updateTheme({ wallpaper: cosmicLightBackground })}
-          selected={wallpaper === cosmicLightBackground}
-          style={{ display: 'inline', marginBottom: '.5em' }}
-        >
-          {__('Cosmic light')}
-        </Option>
-        <Option htmlFor={this.fileInputID} selected={customWallpaper}>
-          {customWallpaper ? (
-            <span>
-              {__('Custom wallpaper')}: {wallpaper}
-            </span>
-          ) : (
-            __('Select a custom wallpaper')
-          )}
-        </Option>
-        <input
-          id={this.fileInputID}
-          type="file"
-          accept="image/*"
-          style={{ display: 'none' }}
-          onChange={this.handleFilePick}
-        />
-      </div>
-    );
+    updateTheme({ wallpaper: imagePath });
   }
 }
 
-export default BackgroundPicker;
+export default function BackgroundPicker() {
+  const wallpaper = useSelector((state) => state.theme.wallpaper);
+  const fileInputID = useMemo(newUID, []);
+  const customWallpaper =
+    wallpaper !== starryNightBackground && wallpaper !== cosmicLightBackground;
+
+  return (
+    <div>
+      <Option
+        onClick={() => updateTheme({ wallpaper: starryNightBackground })}
+        selected={wallpaper === starryNightBackground}
+        style={{ display: 'inline', marginBottom: '.5em' }}
+      >
+        {__('Starry night')}
+      </Option>
+      <Option
+        onClick={() => updateTheme({ wallpaper: cosmicLightBackground })}
+        selected={wallpaper === cosmicLightBackground}
+        style={{ display: 'inline', marginBottom: '.5em' }}
+      >
+        {__('Cosmic light')}
+      </Option>
+      <Option htmlFor={fileInputID} selected={customWallpaper}>
+        {customWallpaper ? (
+          <span>
+            {__('Custom wallpaper')}: {wallpaper}
+          </span>
+        ) : (
+          __('Select a custom wallpaper')
+        )}
+      </Option>
+      <input
+        id={fileInputID}
+        type="file"
+        accept="image/*"
+        style={{ display: 'none' }}
+        onChange={handleFilePick}
+      />
+    </div>
+  );
+}
