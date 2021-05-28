f2c6adbc699bd7f07cdedb5269dc88d9a8f1606d
Krysto <quy.p.hoang@gmail.com>
2021-05-28 12:13:58 +0700

    disabled contextIsolation for virtual keyboard

--- src/main/keyboard.js
+++ src/main/keyboard.js
@@ -24,6 +24,7 @@ export async function openVirtualKeyboard(options) {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: false,
+      contextIsolation: false,
     },
   });
 
