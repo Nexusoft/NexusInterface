6ed78df791262ae3fd73f85c43eef4f6320a3025
Krysto <quy.p.hoang@gmail.com>
2021-06-23 19:45:55 +0700

    fetching market data & supply independently

--- src/shared/store/reducers/market.js
+++ src/shared/store/reducers/market.js
@@ -4,7 +4,18 @@ const initialState = null;
 export default (state = initialState, action) => {
   switch (action.type) {
     case TYPE.SET_MARKET_DATA:
-      return action.payload;
+      return {
+        ...state,
+        ...action.payload,
+        marketCap: action.payload?.price * state?.supply || undefined,
+      };
+
+    case TYPE.SET_TOTAL_SUPPLY:
+      return {
+        ...state,
+        supply: action.payload,
+        marketCap: action.payload * state?.price || undefined,
+      };
 
     default:
       return state;
