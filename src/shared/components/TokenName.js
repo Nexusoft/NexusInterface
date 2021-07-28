341e085fded7c047eca90a514e6516a2a7a00307
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-07-28 14:12:51 -0700

    Allow for token.ticker in Token name component

--- src/shared/components/TokenName.js
+++ src/shared/components/TokenName.js
@@ -4,7 +4,7 @@ import NexusAddress from 'components/NexusAddress';
 const getToken = ({ token, account, contract }) => {
   if (token) return token;
   const obj = account || contract;
-  return { name: obj?.token_name, address: obj?.token };
+  return { name: obj?.token_name || obj?.ticker, address: obj?.token };
 };
 
 const trimAddress = (address) => (address ? address.substring(0, 3) + '…' : '');
