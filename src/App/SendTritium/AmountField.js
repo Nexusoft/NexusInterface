ea4ce6983e834fadb97ee36f8c6669ae28d1377a
Krysto <quy.p.hoang@gmail.com>
2021-09-29 17:22:49 +0700

    cleaning up dead code

--- src/App/SendTritium/AmountField.js
+++ src/App/SendTritium/AmountField.js
@@ -1,5 +1,4 @@
 // External
-import { Component } from 'react';
 import { useSelector } from 'react-redux';
 import { Field } from 'redux-form';
 import styled from '@emotion/styled';
@@ -23,25 +22,12 @@ const SendAmountField = styled.div({
   flex: 1,
 });
 
-const SendAmountEqual = styled.div({
-  display: 'flex',
-  alignItems: 'flex-end',
-  padding: '.1em .6em',
-  fontSize: '1.2em',
-});
-
 const SendAllLink = styled(Link)({
   textTransform: 'uppercase',
   marginLeft: '1em',
   fontSize: '.9em',
   verticalAlign: 'middle',
 });
-
-const TokenAddress = styled.a(({ theme }) => ({
-  textTransform: 'none',
-  marginLeft: '1em',
-  color: theme.mixer(0.5),
-}));
 
 const nxsToFiat = (value, price) => {
   if (price) {
@@ -69,23 +55,6 @@ function AmountTextField({ input, source, ...rest }) {
 
 export default function AmountField({ source, parentFieldName, change }) {
   const fullAmount = (source?.account || source?.token)?.balance;
-  // const fiatCurrency = useSelector(state => state.settings.fiatCurrency)
-
-  // /**
-  //  * Returns the fiat from NXS
-  //  *
-  //  * @memberof AmountField
-  //  */
-  // fiatToNxs = (e, value) => {
-  //   if (floatRegex.test(value)) {
-  //     const fiat = parseFloat(value);
-  //     const { nxsFiatPrice } = this.props;
-  //     if (nxsFiatPrice) {
-  //       const nxs = fiat / nxsFiatPrice;
-  //       this.props.change(this.amountFieldName(), nxs.toFixed(5));
-  //     }
-  //   }
-  // };
 
   const amountFieldName = () =>
     (parentFieldName ? parentFieldName + '.' : '') + 'amount';
@@ -93,7 +62,6 @@ export default function AmountField({ source, parentFieldName, change }) {
   const sendAll = (evt) => {
     evt.preventDefault();
     change(amountFieldName(), fullAmount);
-    // nxsToFiat(null, fullAmount);
   };
 
   return (
@@ -134,23 +102,6 @@ export default function AmountField({ source, parentFieldName, change }) {
           />
         </FormField>
       </SendAmountField>
-
-      {/* {token.address === '0' ? (
-          <>
-            <SendAmountEqual>=</SendAmountEqual>
-
-            <SendAmountField>
-              <FormField connectLabel label={this.props.fiatCurrency}>
-                <Field
-                  component={TextField.RF}
-                  name={this.fiatAmountFieldName()}
-                  placeholder="0.00"
-                  onChange={this.fiatToNxs}
-                />
-              </FormField>
-            </SendAmountField>
-          </>
-        ) : null} */}
     </SendAmount>
   );
 }
