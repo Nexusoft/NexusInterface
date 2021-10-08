049276420d5efba018fb8c488e2a3769776148d7
Krysto <quy.p.hoang@gmail.com>
2021-10-08 16:42:35 +0700

    refactored & cleaned up Tokens tab components

--- src/App/UserPage/Tokens/NewTokenModal.js
+++ src/App/UserPage/Tokens/NewTokenModal.js
@@ -1,6 +1,4 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import { reduxForm, Field, formValueSelector } from 'redux-form';
+import { reduxForm, Field, Fields } from 'redux-form';
 import styled from '@emotion/styled';
 
 import ControlledModal from 'components/ControlledModal';
@@ -14,7 +12,7 @@ import { loadAccounts, loadOwnedTokens } from 'lib/user';
 import { openModal, removeModal, showNotification } from 'lib/ui';
 import NewAccountModal from 'components/NewAccountModal';
 
-import { token, createLocalNameFee } from 'lib/fees';
+import { createTokenFee, createLocalNameFee } from 'lib/fees';
 import GA from 'lib/googleAnalytics';
 
 __ = __context('User.Tokens.NewToken');
@@ -25,17 +23,13 @@ const SubLable = styled.span(({ theme }) => ({
   color: theme.mixer(0.5),
 }));
 
-const mapStateToProps = (state) => {
-  const valueSelector = formValueSelector('new_token');
+function FeeMessage({ supply, decimals }) {
+  return __(`There is a %{tokenfee} NXS token creation fee, based on supply`, {
+    tokenfee: createTokenFee(supply, decimals),
+  });
+}
 
-  return {
-    supply: valueSelector(state, 'supply'),
-    decimal: valueSelector(state, 'decimals'),
-  };
-};
-
-@connect(mapStateToProps)
-@reduxForm({
+const formOptions = {
   form: 'new_token',
   destroyOnUnmount: true,
   initialValues: {
@@ -105,81 +99,73 @@ const mapStateToProps = (state) => {
     loadAccounts();
   },
   onSubmitFail: errorHandler(__('Error creating token')),
-})
-class NewTokenModal extends Component {
-  render() {
-    const { handleSubmit, submitting, supply, decimal } = this.props;
-    const tokenCreationFee = token(supply, decimal);
-    return (
-      <ControlledModal
-        assignClose={(closeModal) => {
-          this.closeModal = closeModal;
-        }}
-        maxWidth={400}
-      >
-        <ControlledModal.Header>{__('New Token')}</ControlledModal.Header>
-        <ControlledModal.Body>
-          <form onSubmit={handleSubmit}>
-            {__(
-              `There is a %{tokenfee} NXS token creation fee, based on supply`,
-              {
-                tokenfee: tokenCreationFee,
-              }
-            )}
-            <FormField connectLabel label={__('Token name')}>
-              <>
-                <SubLable>
-                  {__('Name Creation Fee: %{tokenFee} NXS (Optional)', {
-                    tokenFee: createLocalNameFee,
-                  })}
-                </SubLable>
-                <Field
-                  name="name"
-                  component={TextField.RF}
-                  placeholder={__("New tokens's name")}
-                />
-              </>
-            </FormField>
-            <FormField connectLabel label={__('Supply')}>
-              <>
-                <SubLable> {__('Max amount of tokens available')}</SubLable>
-                <Field
-                  name="supply"
-                  component={TextField.RF}
-                  placeholder={'10000'}
-                />
-              </>
-            </FormField>
-            <FormField connectLabel label={__('Decimal')}>
-              <>
-                <SubLable>
-                  {__('Amount of significant digits a token can have')}{' '}
-                </SubLable>
-                <Field
-                  name="decimals"
-                  component={TextField.RF}
-                  placeholder={'4'}
-                />
-              </>
-            </FormField>
+};
 
-            <div className="mt3 flex space-between">
-              <Button
-                onClick={() => {
-                  this.closeModal();
-                }}
-              >
-                {__('Cancel')}
-              </Button>
-              <Button skin="primary" type="submit" disabled={submitting}>
-                {__('Create token')}
-              </Button>
-            </div>
-          </form>
-        </ControlledModal.Body>
-      </ControlledModal>
-    );
-  }
+function NewTokenModal({ handleSubmit, submitting }) {
+  return (
+    <ControlledModal maxWidth={400}>
+      {(closeModal) => (
+        <>
+          <ControlledModal.Header>{__('New Token')}</ControlledModal.Header>
+          <ControlledModal.Body>
+            <form onSubmit={handleSubmit}>
+              <Fields names={['supply', 'decimals']} component={FeeMessage} />
+
+              <FormField connectLabel label={__('Token name')}>
+                <>
+                  <SubLable>
+                    {__('Name Creation Fee: %{tokenFee} NXS (Optional)', {
+                      tokenFee: createLocalNameFee,
+                    })}
+                  </SubLable>
+                  <Field
+                    name="name"
+                    component={TextField.RF}
+                    placeholder={__("New tokens's name")}
+                  />
+                </>
+              </FormField>
+              <FormField connectLabel label={__('Supply')}>
+                <>
+                  <SubLable> {__('Max amount of tokens available')}</SubLable>
+                  <Field
+                    name="supply"
+                    component={TextField.RF}
+                    placeholder={'10000'}
+                  />
+                </>
+              </FormField>
+              <FormField connectLabel label={__('Decimal')}>
+                <>
+                  <SubLable>
+                    {__('Amount of significant digits a token can have')}{' '}
+                  </SubLable>
+                  <Field
+                    name="decimals"
+                    component={TextField.RF}
+                    placeholder={'4'}
+                  />
+                </>
+              </FormField>
+
+              <div className="mt3 flex space-between">
+                <Button
+                  onClick={() => {
+                    closeModal();
+                  }}
+                >
+                  {__('Cancel')}
+                </Button>
+                <Button skin="primary" type="submit" disabled={submitting}>
+                  {__('Create token')}
+                </Button>
+              </div>
+            </form>
+          </ControlledModal.Body>
+        </>
+      )}
+    </ControlledModal>
+  );
 }
 
-export default NewTokenModal;
+export default reduxForm(formOptions)(NewTokenModal);
--- src/shared/lib/fees.js
+++ src/shared/lib/fees.js
@@ -1,6 +1,6 @@
 export const asset = (bytes) => 0.01 * bytes;
 
-export const token = (supply, decimal) => {
+export const createTokenFee = (supply, decimal) => {
   const totalSupply = supply * Math.pow(10, decimal || 0);
   const fee = (Math.log10(totalSupply) - 2) * 100;
   return fee ? (fee <= 0 ? 1 : fee) : 1;
