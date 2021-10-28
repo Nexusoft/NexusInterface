69307740f33ef9f3ef49d9adff1939dd778dc67a
Krysto <quy.p.hoang@gmail.com>
2021-10-28 12:16:09 +0700

    formSubmit helper function

--- src/shared/lib/form.js
+++ src/shared/lib/form.js
@@ -1,5 +1,8 @@
+import { FORM_ERROR } from 'final-form';
+
 import { UPDATE_FORM_INSTANCE } from 'consts/actionTypes';
 import store from 'store';
+import { openErrorDialog } from 'lib/dialog';
 
 export function updateFormInstance(formName, instance) {
   store.dispatch({
@@ -10,3 +13,26 @@ export function updateFormInstance(formName, instance) {
 
 export const selectFormInstance = (formName) => (state) =>
   state.forms[formName];
+
+export const formSubmit =
+  ({ preSubmit, submit, onSuccess, errorMessage }) =>
+  async (values, form) => {
+    if (preSubmit) {
+      const proceed = preSubmit(values, form);
+      if (!proceed) return;
+    }
+
+    try {
+      const result = await Promise.resolve(submit(values, form));
+    } catch (err) {
+      openErrorDialog({
+        message: errorMessage || __('Error'),
+        note:
+          err?.message || (typeof err === 'string' ? err : __('Unknown error')),
+      });
+      return {
+        [FORM_ERROR]: err,
+      };
+    }
+    onSuccess?.(result, values, form);
+  };
