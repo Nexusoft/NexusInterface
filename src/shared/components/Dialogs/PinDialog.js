556a8c1e2b0a0fbceb1900796be7260371bf5f91
Krysto <quy.p.hoang@gmail.com>
2021-11-04 12:00:54 +0700

    Pin dialog using the new Form

--- src/shared/components/Dialogs/PinDialog.js
+++ src/shared/components/Dialogs/PinDialog.js
@@ -1,14 +1,13 @@
-import { reduxForm, Field } from 'redux-form';
 import styled from '@emotion/styled';
 
 import ControlledModal from 'components/ControlledModal';
-import TextFieldWithKeyboard from 'components/TextFieldWithKeyboard';
+import Form from 'components/Form';
 import Button from 'components/Button';
-import { removeModal } from 'lib/ui';
+import { formSubmit } from 'lib/form';
 
 __ = __context('PinDialog');
 
-const PinInput = styled(TextFieldWithKeyboard.RF)({
+const PinInput = styled(Form.TextFieldWithKeyboard)({
   margin: '1em auto 2.5em',
   fontSize: 18,
 });
@@ -18,59 +17,59 @@ const Note = styled.div({
   marginBottom: '1.5em',
 });
 
-const formOptions = {
-  form: 'pin',
-  destroyOnUnmount: true,
-  initialValues: {
-    pin: '',
-  },
-  validate: ({ pin }) => {
-    const errors = {};
-    if (!pin || pin.length < 4) {
-      errors.pin = __('Pin must be at least 4 characters');
-    }
-    return errors;
-  },
-  onSubmit: ({ pin }, dispatch, props) => {
-    if (props.submitPin) {
-      props.submitPin(pin);
-    }
-    removeModal(props.modalId);
-  },
-};
-
-const PinDialog = ({
-  handleSubmit,
+export default function PinDialog({
+  submitPin,
   note = null,
   confirmLabel = __('Confirm'),
   onClose,
-}) => (
-  <ControlledModal maxWidth={350} onClose={onClose}>
-    {(closeModal) => (
-      <>
-        <ControlledModal.Header>{__('Enter PIN')}</ControlledModal.Header>
-        <ControlledModal.Body>
-          <form onSubmit={handleSubmit}>
-            <Field
-              component={PinInput}
-              maskable
-              name="pin"
-              autoFocus
-              skin="filled-inverted"
-              placeholder={__('Your PIN')}
-            />
-            {!!note && <Note>{note}</Note>}
-            <div className="flex space-between">
-              <Button onClick={closeModal}>{__('Cancel')}</Button>
-              <Button type="submit" skin="primary">
-                {confirmLabel}
-              </Button>
-            </div>
-          </form>
-        </ControlledModal.Body>
-      </>
-    )}
-  </ControlledModal>
-);
+}) {
+  return (
+    <ControlledModal maxWidth={350} onClose={onClose}>
+      {(closeModal) => {
+        const formOptions = {
+          name: 'pin',
+          initialValues: {
+            pin: '',
+          },
+          validate: ({ pin }) => {
+            const errors = {};
+            if (!pin || pin.length < 4) {
+              errors.pin = __('Pin must be at least 4 characters');
+            }
+            return errors;
+          },
+          onSubmit: formSubmit({
+            submit: ({ pin }) => {
+              submitPin?.(pin);
+            },
+            onSuccess: closeModal,
+          }),
+        };
 
-export default reduxForm(formOptions)(PinDialog);
+        return (
+          <>
+            <ControlledModal.Header>{__('Enter PIN')}</ControlledModal.Header>
+            <ControlledModal.Body>
+              <Form {...formOptions}>
+                <PinInput
+                  name="pin"
+                  maskable
+                  autoFocus
+                  skin="filled-inverted"
+                  placeholder={__('Your PIN')}
+                />
+                {!!note && <Note>{note}</Note>}
+                <div className="flex space-between">
+                  <Button onClick={closeModal}>{__('Cancel')}</Button>
+                  <Form.SubmitButton skin="primary">
+                    {confirmLabel}
+                  </Form.SubmitButton>
+                </div>
+              </Form>
+            </ControlledModal.Body>
+          </>
+        );
+      }}
+    </ControlledModal>
+  );
+}
