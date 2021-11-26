f96c23a9d68b1b2510cc08cf06670d0e6955de26
Krysto <quy.p.hoang@gmail.com>
2021-11-26 13:43:05 +0700

    improved useUID hook

--- src/shared/utils/useUID.js
+++ src/shared/utils/useUID.js
@@ -1,11 +1,9 @@
-import { useRef, useMemo } from 'react';
+import { useState } from 'react';
 import { newUID } from 'utils/misc';
 
 export default function useUID() {
-  const ref = useRef();
-  // Use useMemo to generate newUID before the first render, useEffect only run after the first render
-  // But we can't rely solely on useMemo because it doesn't ensure memoization https://reactjs.org/docs/hooks-reference.html#usememo
-  // So we still have to use a ref to keep the UID value consistent
-  const uid = useMemo(() => ref.current || newUID(), []);
-  return uid;
+  // Passing a function into `useState` as `initialState`
+  // It will only be executed in the first render
+  const [ref] = useState(newUID);
+  return ref;
 }
