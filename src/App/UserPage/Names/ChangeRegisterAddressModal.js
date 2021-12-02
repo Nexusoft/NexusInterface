470db5a45b86098d438d34109a4bf5ee37828a91
Krysto <quy.p.hoang@gmail.com>
2021-12-02 13:14:17 +0700

    converted ChangeRegisterAddress modal to the new form system

--- src/App/UserPage/Names/ChangeRegisterAddressModal.js
+++ src/App/UserPage/Names/ChangeRegisterAddressModal.js
@@ -1,17 +1,15 @@
 import { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
 import styled from '@emotion/styled';
 
 import ControlledModal from 'components/ControlledModal';
-import Button from 'components/Button';
 import FormField from 'components/FormField';
-import TextField from 'components/TextField';
 import Spinner from 'components/Spinner';
+import { formSubmit } from 'lib/form';
 import { confirmPin, openSuccessDialog } from 'lib/dialog';
-import { errorHandler } from 'utils/form';
 import { loadNameRecords, selectUsername } from 'lib/user';
 import { callApi } from 'lib/tritiumApi';
+import memoize from 'utils/memoize';
 
 __ = __context('ChangeRegisterAddress');
 
@@ -23,32 +21,11 @@ const Name = styled.span(({ theme }) => ({
   color: theme.foreground,
 }));
 
-const formOptions = {
-  form: 'change-register-address',
-  destroyOnUnmount: true,
-  onSubmit: async ({ registerAddress }, dispatch, { nameRecord }) => {
-    const pin = await confirmPin();
+const getInitialValues = memoize((registerAddress) => ({
+  registerAddress,
+}));
 
-    if (pin) {
-      return await callApi('names/update/name', {
-        pin,
-        address: nameRecord.address,
-        register: registerAddress,
-      });
-    }
-  },
-  onSubmitSuccess: async (result, dispatch, props) => {
-    if (!result) return; // Submission was cancelled
-    loadNameRecords();
-    props.closeModal();
-    openSuccessDialog({
-      message: __('Name has been updated'),
-    });
-  },
-  onSubmitFail: errorHandler(__('Error updating name')),
-};
-
-function ChangeRegisterAddressForm({ handleSubmit, nameRecord, submitting }) {
+export default function ChangeRegisterAddressModal({ nameRecord }) {
   const username = useSelector(selectUsername);
   const inputRef = useRef();
   useEffect(() => {
@@ -59,54 +36,6 @@ function ChangeRegisterAddressForm({ handleSubmit, nameRecord, submitting }) {
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormField label={__('Name')}>
-        {nameRecord.global ? null : nameRecord.namespace ? (
-          <Prefix>{nameRecord.namespace + '::'}</Prefix>
-        ) : (
-          <Prefix>{username + ':'}</Prefix>
-        )}
-        <Name>{nameRecord.name}</Name>
-      </FormField>
-
-      <FormField connectLabel label={__('Register address')}>
-        <Field
-          inputRef={inputRef}
-          name="registerAddress"
-          component={TextField.RF}
-          placeholder={__('Register address that this name points to')}
-        />
-      </FormField>
-
-      <div className="mt1" style={{ textAlign: 'left' }}>
-        {__('Name update fee')}: 1 NXS
-      </div>
-
-      <Button
-        skin="primary"
-        wide
-        uppercase
-        className="mt3"
-        type="submit"
-        disabled={submitting}
-      >
-        {submitting ? (
-          <span>
-            <Spinner className="mr0_4" />
-            <span className="v-align">{__('Updating name')}...</span>
-          </span>
-        ) : (
-          __('Update name')
-        )}
-      </Button>
-    </form>
-  );
-}
-
-ChangeRegisterAddressForm = reduxForm(formOptions)(ChangeRegisterAddressForm);
-
-export default function ChangeRegisterAddressModal({ nameRecord }) {
-  return (
     <ControlledModal maxWidth={600}>
       {(closeModal) => (
         <>
@@ -114,13 +43,65 @@ export default function ChangeRegisterAddressModal({ nameRecord }) {
             {__('Change register address')}
           </ControlledModal.Header>
           <ControlledModal.Body>
-            <ChangeRegisterAddressForm
-              closeModal={closeModal}
-              nameRecord={nameRecord}
-              initialValues={{
-                registerAddress: nameRecord.register,
-              }}
-            />
+            <Form
+              name="change-register-address"
+              initialValues={getInitialValues(nameRecord.register)}
+              onSubmit={formSubmit({
+                submit: async ({ registerAddress }) => {
+                  const pin = await confirmPin();
+                  if (pin) {
+                    return await callApi('names/update/name', {
+                      pin,
+                      address: nameRecord.address,
+                      register: registerAddress,
+                    });
+                  }
+                },
+                onSuccess: async (result) => {
+                  if (!result) return; // Submission was cancelled
+                  loadNameRecords();
+                  closeModal();
+                  openSuccessDialog({
+                    message: __('Name has been updated'),
+                  });
+                },
+                errorMessage: __('Error updating name'),
+              })}
+            >
+              <FormField label={__('Name')}>
+                {nameRecord.global ? null : nameRecord.namespace ? (
+                  <Prefix>{nameRecord.namespace + '::'}</Prefix>
+                ) : (
+                  <Prefix>{username + ':'}</Prefix>
+                )}
+                <Name>{nameRecord.name}</Name>
+              </FormField>
+
+              <FormField connectLabel label={__('Register address')}>
+                <Form.TextField
+                  inputRef={inputRef}
+                  name="registerAddress"
+                  placeholder={__('Register address that this name points to')}
+                />
+              </FormField>
+
+              <div className="mt1" style={{ textAlign: 'left' }}>
+                {__('Name update fee')}: 1 NXS
+              </div>
+
+              <Form.SubmitButton skin="primary" wide uppercase className="mt3">
+                {({ submitting }) =>
+                  submitting ? (
+                    <span>
+                      <Spinner className="mr0_4" />
+                      <span className="v-align">{__('Updating name')}...</span>
+                    </span>
+                  ) : (
+                    __('Update name')
+                  )
+                }
+              </Form.SubmitButton>
+            </Form>
           </ControlledModal.Body>
         </>
       )}
