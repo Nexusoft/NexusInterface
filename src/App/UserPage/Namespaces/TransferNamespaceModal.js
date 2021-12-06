cdd5d2f10a06c23a5477fa85b7cae3b03657ee72
Krysto <quy.p.hoang@gmail.com>
2021-12-06 11:19:58 +0700

    converted TransferNamespace modal to the new form system

--- src/App/UserPage/Namespaces/TransferNamespaceModal.js
+++ src/App/UserPage/Namespaces/TransferNamespaceModal.js
@@ -1,14 +1,11 @@
-import { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
 import styled from '@emotion/styled';
 
+import Form from 'components/Form';
 import ControlledModal from 'components/ControlledModal';
-import Button from 'components/Button';
 import FormField from 'components/FormField';
-import TextField from 'components/TextField';
 import Spinner from 'components/Spinner';
+import { formSubmit, required } from 'lib/form';
 import { confirmPin, openSuccessDialog } from 'lib/dialog';
-import { errorHandler } from 'utils/form';
 import { loadNamespaces } from 'lib/user';
 import { callApi } from 'lib/tritiumApi';
 import { userIdRegex } from 'consts/misc';
@@ -19,83 +16,9 @@ const Namespace = styled.span(({ theme }) => ({
   color: theme.foreground,
 }));
 
-@reduxForm({
-  form: 'transfer-namespace',
-  destroyOnUnmount: true,
-  initialValues: {
-    recipient: '',
-  },
-  validate: ({ recipient }) => {
-    const errors = {};
-    if (!recipient) {
-      errors.recipient = __('Destination is required');
-    }
-    return errors;
-  },
-  onSubmit: async ({ recipient }, dispatch, { namespace }) => {
-    const pin = await confirmPin();
-
-    const params = { pin, address: namespace.address };
-    if (userIdRegex.test(recipient)) {
-      params.destination = recipient;
-    } else {
-      params.username = recipient;
-    }
-
-    if (pin) {
-      return await callApi('names/transfer/namespace', params);
-    }
-  },
-  onSubmitSuccess: async (result, dispatch, props) => {
-    if (!result) return; // Submission was cancelled
-    loadNamespaces();
-    props.closeModal();
-    openSuccessDialog({
-      message: __('Namespace has been transferred'),
-    });
-  },
-  onSubmitFail: errorHandler(__('Error transferring namespace')),
-})
-class TransferNamespaceForm extends Component {
-  render() {
-    const { handleSubmit, namespace, submitting } = this.props;
-    return (
-      <form onSubmit={handleSubmit}>
-        <FormField label={__('Namespace')}>
-          <Namespace>{namespace.name}</Namespace>
-        </FormField>
-
-        <FormField connectLabel label={__('Transfer to')}>
-          <Field
-            inputRef={this.inputRef}
-            name="recipient"
-            autoFocus
-            component={TextField.RF}
-            placeholder={__('Recipient username or user ID')}
-          />
-        </FormField>
-
-        <Button
-          skin="primary"
-          wide
-          uppercase
-          className="mt3"
-          type="submit"
-          disabled={submitting}
-        >
-          {submitting ? (
-            <span>
-              <Spinner className="mr0_4" />
-              <span className="v-align">{__('Transferring namespace')}...</span>
-            </span>
-          ) : (
-            __('Transfer namespace')
-          )}
-        </Button>
-      </form>
-    );
-  }
-}
+const initialValues = {
+  recipient: '',
+};
 
 export default function TransferNamespaceModal({ namespace }) {
   return (
@@ -106,10 +29,63 @@ export default function TransferNamespaceModal({ namespace }) {
             {__('Transfer namespace')}
           </ControlledModal.Header>
           <ControlledModal.Body>
-            <TransferNamespaceForm
-              closeModal={closeModal}
-              namespace={namespace}
-            />
+            <Form
+              name="transfer-namespace"
+              initialValues={initialValues}
+              onSubmit={formSubmit({
+                submit: async ({ recipient }) => {
+                  const pin = await confirmPin();
+
+                  const params = { pin, address: namespace.address };
+                  if (userIdRegex.test(recipient)) {
+                    params.destination = recipient;
+                  } else {
+                    params.username = recipient;
+                  }
+
+                  if (pin) {
+                    return await callApi('names/transfer/namespace', params);
+                  }
+                },
+                onSuccess: async (result) => {
+                  if (!result) return; // Submission was cancelled
+                  loadNamespaces();
+                  closeModal();
+                  openSuccessDialog({
+                    message: __('Namespace has been transferred'),
+                  });
+                },
+                errorMessage: __('Error transferring namespace'),
+              })}
+            >
+              <FormField label={__('Namespace')}>
+                <Namespace>{namespace.name}</Namespace>
+              </FormField>
+
+              <FormField connectLabel label={__('Transfer to')}>
+                <Form.TextField
+                  name="recipient"
+                  autoFocus
+                  placeholder={__('Recipient username or user ID')}
+                  validate={required()}
+                />
+              </FormField>
+
+              <Form.SubmitButton skin="primary" wide uppercase className="mt3">
+                {({ submitting }) =>
+                  submitting ? (
+                    <span>
+                      <Spinner className="mr0_4" />
+                      <span className="v-align">
+                        {__('Transferring namespace')}...
+                      </span>
+                    </span>
+                  ) : (
+                    __('Transfer namespace')
+                  )
+                }
+              </Form.SubmitButton>
+            </Form>
           </ControlledModal.Body>
         </>
       )}
