301ba74e79dd38133a647d1bc80ca523f85fc623
Krysto <quy.p.hoang@gmail.com>
2022-06-22 17:12:47 +0700

    just session

--- src/shared/lib/session.js
+++ src/shared/lib/session.js
@@ -6,11 +6,11 @@ const cacheStaleTime = 1000 * 60 * 60 * 24 * 7; // 7 days
 export function loadSessionCache() {
   const cacheJson = localStorage.getItem(localStorageKey);
   const cache = tryParsingJson(cacheJson) || {};
-  for (const sessionId in cache) {
-    if (cache?.hasOwnProperty(sessionId)) {
-      if (Date.now() - cacheStaleTime > cache[sessionId].time) {
-        console.log('[Session cache] I found extreme stale key', sessionId);
-        delete cache[sessionId];
+  for (const session in cache) {
+    if (cache?.hasOwnProperty(session)) {
+      if (Date.now() - cacheStaleTime > cache[session].time) {
+        console.log('[Session cache] I found extreme stale key', session);
+        delete cache[session];
       }
     }
   }
@@ -21,8 +21,11 @@ export function saveSessionCache(cache) {
   localStorage.setItem(localStorageKey, JSON.stringify(cache));
 }
 
-export function addSession({ sessionId, username }) {
+/**
+ * Save session information including username into localStorage
+ */
+export function addSession({ session, username }) {
   const cache = loadSessionCache();
-  cache[sessionId] = { username, time: Date.now() };
+  cache[session] = { username, time: Date.now() };
   saveSessionCache(cache);
 }
