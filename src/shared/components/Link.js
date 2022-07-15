89cd369ef016bf0e14d109ce362c20101fe105f9
Krysto <quy.p.hoang@gmail.com>
2022-07-15 11:33:36 +0700

    removed warning comment in Link.js

--- src/shared/components/Link.js
+++ src/shared/components/Link.js
@@ -1,14 +1,3 @@
-/**
- * Important note - This file is imported into module_preload.js, either directly or
- * indirectly, and will be a part of the preload script for modules, therefore:
- * - Be picky with importing stuffs into this file, especially for big
- * files and libraries. The bigger the preload scripts get, the slower the modules
- * will load.
- * - Don't assign anything to `global` variable because it will be passed
- * into modules' execution environment.
- * - Make sure this note also presents in other files which are imported here.
- */
-
 // External
 import { forwardRef } from 'react';
 import styled from '@emotion/styled';
