4d589def45a989176a6ffff64c1cc9faa96479e1
Krysto <quy.p.hoang@gmail.com>
2023-03-03 09:57:27 +0700

    added name reverse lookup to RecipientAddress

--- src/App/SendTritium/RecipientAddress.js
+++ src/App/SendTritium/RecipientAddress.js
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import NexusAddress from 'components/NexusAddress';
-
+import Icon from 'components/Icon';
+import { lookupAddress } from 'lib/addressBook';
+import { callApi } from 'lib/tritiumApi';
 import { addressRegex } from 'consts/misc';
 import { debounced } from 'utils/universal';
+import contactIcon from 'icons/address-book.svg';
 
 const resolveName = debounced(async (name, callback) => {
   try {
@@ -13,6 +16,34 @@ const resolveName = debounced(async (name, callback) => {
     callback(null);
   }
 }, 500);
+
+function useAddressLabel(address) {
+  const [name, setName] = useState(null);
+  const contact = lookupAddress(address);
+  useEffect(() => {
+    if (!contact) {
+      callApi('names/lookup/address', { address })
+        .then(({ name }) => setName(name))
+        .catch();
+    } else if (label) {
+      setName(null);
+    }
+  }, [address]);
+
+  if (contact) {
+    return (
+      <span>
+        <Icon icon={contactIcon} className="mr0_4" />
+        <span className="v-align">
+          {contact.name}
+          {contact.label ? ` - ${contact.label}` : ''}
+        </span>
+      </span>
+    );
+  } else {
+    return name;
+  }
+}
 
 export default function RecipientAddress({
   nameOrAddress,
@@ -32,6 +63,11 @@ export default function RecipientAddress({
       resolveName(nameOrAddress, setAddress);
     }
   }, [nameOrAddress]);
+  const label = useAddressLabel(address);
 
-  return !!address && <NexusAddress address={address} />;
+  return (
+    !!address && (
+      <NexusAddress label={<span>Send to {label}</span>} address={address} />
+    )
+  );
 }
